3811c8e322b4c3f9ccf7fa43a1e4021f2769f6cb
Türei Dénes <turei.denes@gmail.com>
2016-05-26 13:12:42 +0100

    Mac install with brew

---
+++ mac-install-brew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright (c) 2014-2016 - EMBL-EBI
+#
+#  File author(s): Dénes Türei (denes@ebi.ac.uk)
+#
+#  Distributed under the GNU GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+#
+#  Tested on OS X 10.11.3
+#
+
+# installing HomeBrew first:
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
