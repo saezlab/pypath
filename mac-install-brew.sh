aa43ee7ab33405a8aeda9a58a7a28a6642999eda
Türei Dénes <turei.denes@gmail.com>
2016-05-26 15:06:30 +0100

    brew install pip packages

--- mac-install-brew.sh
+++ mac-install-brew.sh
@@ -23,12 +23,31 @@ USER=`whoami`
 HOME="/Users/$USER"
 LOCAL="$HOME/local"
 LOCALBIN="$LOCAL/bin"
+LOCALPIP="$LOCALBIN/pip2.7"
+PYPATHURL="http://pypath.omnipathdb.org/releases/latest/pypath-latest.tar.gz"
 
-if ![ -d $LOCAL ];
+if [ ! -d $LOCAL ];
     then mkdir $LOCAL;
 fi
+
+export PATH="$LOCALBIN:$PATH"
 
 cd $LOCAL
 
 # downloading and extracting homebrew:
 curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1
+
+brew update
+brew install python py2cairo homebrew/science/igraph graphviz
+
+$LOCALPIP install --upgrade pip
+$LOCALPIP install python-igraph
+$LOCALPIP install pysftp
+$LOCALPIP install fabric
+$LOCALPIP install pandas
+$LOCALPIP install suds-jurko
+$LOCALPIP install bioservices
+$LOCALPIP install pymysql
+$LOCALPIP install pygraphviz
+
+$LOCALPIP install $PYPATHURL
