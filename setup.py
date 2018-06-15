e750225d41209695f53a454154c24053f07ca470
Türei Dénes <turei.denes@gmail.com>
2018-06-15 18:09:22 +0200

    pip.main issue fixed in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 #
 #  This file is part of the `pypath` python module
 #
-#  Copyright (c) 2014-2017 - EMBL-EBI
+#  Copyright (c) 2014-2018 - EMBL
 #
-#  File author(s): Dénes Türei (denes@ebi.ac.uk)
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
 #
 #  Distributed under the GPLv3 License.
 #  See accompanying file LICENSE.txt or copy at
@@ -32,7 +32,7 @@ class __install(_install):
     
     def run(self):
         
-        pip.main(['install', 'git+git://github.com/fabioticconi/fishers_exact_test@fix-setup-deps'])
+        pip._internal.main(['install', 'git+git://github.com/fabioticconi/fishers_exact_test@fix-setup-deps'])
         
         _install.run(self)
 
