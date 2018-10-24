63434363bb83fcd9f7fbcbc251cb7391bf5a0c60
Türei Dénes <turei.denes@gmail.com>
2018-10-24 19:57:20 +0200

    script to remove jupyter notebook header

---
+++ src/remove_notebook_header.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Jupyter is unable to apply the syntax highlighting colors from
+# custom themes in jupyterthemes.
+# By saving the notebook in the browser with save as the colors
+# are preserved but the header remains and the scrolling will be
+# disabled.
+# These issues can be resolved the way below.
+
+if [ ! -f "$1" ];
+then
+	echo "Usage: $0 <notebook_saved_from_browser.html>"
+	exit -1
+fi
+
+sed -iE ':a;s/<!--.*-->//g;/<!--/{N;ba};'\
+'s/@font-face {.*}//g;'\
+'s/<html/<html style="height:100%;"/;'\
+'s/<body/<body style="height:100%;"/;'\
+'s/\(<div id="header"\)/<!-- \1/;'\
+'s/\(<div id="site" style=".*">\)/\1 -->/;'\
+'s/\(id="notebook_panel"\) style="overflow:auto!important;"/\1/g;'\
+'s/\(id="notebook_panel"\)/\1 style="overflow:auto!important;"/g;'\
+ "$1"
