1040bc52cf45cf68036ad6d0706831714f5b64ca
Türei Dénes <turei.denes@gmail.com>
2019-01-08 21:25:35 +0100

    new module: pypath.annot

---
+++ src/pypath/annot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2018
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+from future.utils import iteritems
+from past.builtins import xrange, range, reduce
+
+
+import pypath.dataio as dataio
