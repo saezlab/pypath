660b8b710b8f5b116bb9b1dc341ebf5aefc73926
Türei Dénes <turei.denes@gmail.com>
2019-02-26 15:51:29 +0100

    cellphonedb git urls

--- src/pypath/urls.py
+++ src/pypath/urls.py
@@ -881,6 +881,15 @@ urls = {
         'proteins': 'https://www.cellphonedb.org/downloads/'\
             'protein_complex_cellphonedb.csv',
     },
+    'cellphonedb_git': {
+        'label': 'Intercellular communication database from Teichmann lab',
+        'proteins': 'https://github.com/Teichlab/cellphonedb/blob/'\
+            'master/cellphonedb/src/core/data/protein.csv',
+        'complexes': 'https://github.com/Teichlab/cellphonedb/blob/'\
+            'master/cellphonedb/src/core/data/complex.csv',
+        'interactions': 'https://github.com/Teichlab/cellphonedb/blob/'
+            'master/cellphonedb/src/core/data/interaction.csv',
+    }
     'stitch': {
         'label': 'The STITCH small molecule-protein interaction database',
         'actions': 'http://stitch.embl.de/download/actions.v5.0/'\
