41fa3436a4da9e680344ddfee2bab10c120b1772
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-03-13 10:58:51 +0100

    Update docstring

--- src/pypath/bel.py
+++ src/pypath/bel.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Convert OmniPath components to PyBEL."""
+"""Convert OmniPath components to PyBEL.
 
-#
-#  This file is part of the `pypath` python module
-#
-#  Copyright
-#  2014-2019
-#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
-#
-#  File author(s): Dénes Türei (turei.denes@gmail.com)
-#                  Nicolàs Palacio
-#
-#  Distributed under the GPLv3 License.
-#  See accompanying file LICENSE.txt or copy at
-#      http://www.gnu.org/licenses/gpl-3.0.html
-#
-#  Website: http://pypath.omnipathdb.org/
-#
+This file is part of the `pypath` python module
 
+Copyright
+2014-2019
+EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+
+File author(s): Dénes Türei (turei.denes@gmail.com)
+                 Nicolàs Palacio
+
+Distributed under the GPLv3 License.
+See accompanying file LICENSE.txt or copy at
+     http://www.gnu.org/licenses/gpl-3.0.html
+
+Website: http://pypath.omnipathdb.org/
+"""
 
 import itertools as itt
 import sys
