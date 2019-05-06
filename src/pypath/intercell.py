87f03fd1df660c8968a78f69d6753a150156345e
Türei Dénes <turei.denes@gmail.com>
2019-05-06 03:44:37 +0200

    intercell: adding category types and parent categories to df

--- src/pypath/intercell.py
+++ src/pypath/intercell.py
@@ -20,6 +20,10 @@
 
 import imp
 import collections
+import itertools
+
+import numpy as np
+import pandas as pd
 
 import pypath.session_mod as session_mod
 import pypath.annot as annot
@@ -44,6 +48,71 @@ class IntercellAnnotation(annot.CustomAnnotation):
         annot.CustomAnnotation.__init__(
             self,
             class_definitions = class_definitions,
+        )
+        
+        self.make_df()
+        self.set_classes()
+        self.add_classes_to_df()
+    
+    
+    def reload(self):
+        """
+        Reloads the object from the module level.
+        """
+        
+        modname = self.__class__.__module__
+        mod = __import__(modname, fromlist = [modname.split('.')[0]])
+        imp.reload(mod)
+        new = getattr(mod, self.__class__.__name__)
+        setattr(self, '__class__', new)
+    
+    
+    def set_classes(self):
+        
+        self.class_names = set(itertools.chain(
+            *intercell_annot.class_types.values()
+        ))
+        self.class_types = dict(
+            (cls, typ)
+            for typ, ccls in intercell_annot.class_types.items()
+            for cls in ccls
+        )
+        
+        self.main_classes = {}
+        
+        for cls in set(self.df.category):
+            
+            mainclass = None
+            
+            cls_split = cls.split('_')
+            
+            for j in range(len(cls_split)):
+                
+                this_part = '_'.join(cls_split[:j])
+                
+                if this_part in self.class_names:
+                    
+                    mainclass = this_part
+            
+            self.main_classes[cls] = mainclass
+    
+    
+    def add_classes_to_df(self):
+        
+        self.df['mainclass'] = (
+            pd.Series(np.array([
+                self.main_classes[c] for c in self.df.category
+            ])).values
+        )
+        self.df['class_type'] = (
+            pd.Series(np.array([
+                (
+                    self.class_types[c]
+                        if c in self.class_types else
+                    'sub'
+                )
+                for c in self.df.category
+            ])).values
         )
 
 
