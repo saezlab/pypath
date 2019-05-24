153fafe64d4922c63402620db4d88e998ecea93e
Nico <nicolaspalacio91@gmail.com>
2019-05-24 14:00:28 +0200

    Added pass to empty method
    Avoids SyntaxError when importing

--- src/pypath/intercell.py
+++ src/pypath/intercell.py
@@ -39,38 +39,38 @@ IntercellRole = collections.namedtuple(
 
 
 class IntercellAnnotation(annot.CustomAnnotation):
-    
-    
+
+
     def __init__(self, class_definitions = None):
-        
+
         class_definitions = (
             class_definitions or intercell_annot.annot_combined_classes
         )
-        
+
         annot.CustomAnnotation.__init__(
             self,
             class_definitions = class_definitions,
         )
-        
+
         self.make_df()
         self.set_classes()
         self.add_classes_to_df()
-    
-    
+
+
     def reload(self):
         """
         Reloads the object from the module level.
         """
-        
+
         modname = self.__class__.__module__
         mod = __import__(modname, fromlist = [modname.split('.')[0]])
         imp.reload(mod)
         new = getattr(mod, self.__class__.__name__)
         setattr(self, '__class__', new)
-    
-    
+
+
     def set_classes(self):
-        
+
         self.class_names = set(itertools.chain(
             *intercell_annot.class_types.values()
         ))
@@ -79,28 +79,28 @@ class IntercellAnnotation(annot.CustomAnnotation):
             for typ, ccls in intercell_annot.class_types.items()
             for cls in ccls
         )
-        
+
         self.main_classes = {}
-        
+
         for cls in set(self.df.category):
-            
+
             mainclass = None
-            
+
             cls_split = cls.split('_')
-            
+
             for j in range(len(cls_split)):
-                
+
                 this_part = '_'.join(cls_split[:j])
-                
+
                 if this_part in self.class_names:
-                    
+
                     mainclass = this_part
-            
+
             self.main_classes[cls] = mainclass
-    
-    
+
+
     def add_classes_to_df(self):
-        
+
         self.df['mainclass'] = (
             pd.Series(np.array([
                 self.main_classes[c] for c in self.df.category
@@ -119,8 +119,8 @@ class IntercellAnnotation(annot.CustomAnnotation):
 
 
 class Intercell(IntercellAnnotation):
-    
-    
+
+
     def __init__(
             self,
             class_definitions = None,
@@ -128,62 +128,61 @@ class Intercell(IntercellAnnotation):
             network_param = None,
             omnipath = False,
         ):
-        
+
         session_mod.Logger.__init__(self, name = 'intercell')
-        
+
         IntercellAnnotation.__init__(
             self,
             class_definitions = class_definitions,
         )
-        
+
         self.network = network
         self.network_param = network_param or {}
         self.omnipath = omnipath
-    
-    
+
+
     def reload(self):
-        
+
         modname = self.__class__.__module__
         mod = __import__(modname, fromlist = [modname.split('.')[0]])
         import imp
         imp.reload(mod)
         new = getattr(mod, self.__class__.__name__)
         setattr(self, '__class__', new)
-    
-    
+
+
     def main(self):
-        
+
         self.setup_network()
         self.setup_complex()
-    
-    
+
+
     def setup_network(self):
-        
+
         if self.network is None:
-            
+
             if not self.omnipath and 'lst' not in self.network_param:
-                
+
                 self.network_param['lst'] = data_formats.pathway
-            
+
             self.network = main_mod.PyPath()
-            
+
             if self.omnipath:
-                
+
                 self.network.load_omnipath(**self.network_param)
-                
+
             else:
-                
+
                 self.network.init_network(**self.network_param)
-        
+
         if isinstance(self.network, main_mod.PyPath):
-            
+
             self.network = network_mod.Network(records = self.network)
-            
+
         if isinstance(self.network, network_mod.Network):
-            
+
             pass
-    
-    
+
+
     def setup_complex(self):
-        
-        
+        pass
