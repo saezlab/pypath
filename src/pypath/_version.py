a4aafcd2d1852686e3fb4455df8b7b9b0dffe417
Türei Dénes <turei.denes@gmail.com>
2019-08-23 00:55:22 +0200

    _version.py little adjustments

--- src/pypath/_version.py
+++ src/pypath/_version.py
@@ -19,10 +19,21 @@
 #  Website: http://pypath.omnipathdb.org/
 #
 
+__all__ = [
+    '_get_version',
+    '__version__',
+    '__release__',
+    '__author__',
+    '_MINOR',
+    '_MAJOR',
+    '_MICRO',
+]
+
 import os
 
 def _get_version():
-    """Returns the version number.
+    """
+    Returns the version number.
 
     Reads file named ``__version__`` and returns it as a tuple of
     integers.
@@ -37,13 +48,22 @@ def _get_version():
         (1, 11, 0)
     """
 
-    # XXX: Why all caps? this is not a global variable
-    ROOT = os.path.abspath(os.path.dirname(__file__))
+    dir_root = os.path.abspath(os.path.dirname(__file__))
 
-    with open(os.path.join(ROOT, '__version__'), 'r') as v:
+    with open(os.path.join(dir_root, '__version__'), 'r') as v:
+        
         return tuple([int(i) for i in v.read().strip().split('.')])
+
 
 _MAJOR, _MINOR, _MICRO = _get_version()
 __version__ = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 __release__ = '%d.%d' % (_MAJOR, _MINOR) # XXX: Not used
-__author__ = u'Dénes Türei'
+__author__ = (
+    u'Dénes Türei, '
+    u'Nicolàs Palacio, '
+    u'Olga Ivanova. \n'
+    u'European Molecular Biology Laboratory, Heidelberg Germany\n'
+    u'European Bioinformatics Institute, Hinxton UK\n'
+    u'University Hospital RWTH, Aachen Germany\n'
+    u'University Hospital Heidelberg Germany'
+)
