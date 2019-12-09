4ac976e86652eb41af8adc0665bde538e6d57e95
Türei Dénes <turei.denes@gmail.com>
2019-12-09 16:33:28 +0100

    methods for merging evidences

--- src/pypath/evidence.py
+++ src/pypath/evidence.py
@@ -20,6 +20,13 @@
 #  Website: http://pypath.omnipathdb.org/
 #
 
+import pypath.refs as refs
+import pypath.common as common
+import pypath.session_mod as session_mod
+
+_logger = session_mod.Logger(name = 'evidence')
+_log = _logger._log
+
 
 class Evidence(object):
     
@@ -27,4 +34,96 @@ class Evidence(object):
     def __init__(self, resource, references = None):
         
         self.resource = resource
-        self.references = references or set()
+        self.references = self._process_references(references)
+    
+    
+    @staticmethod
+    def _process_references(references):
+        
+        references = common.to_set(references)
+        
+        return (
+            set(
+                (
+                    refs.Reference(ref)
+                        if not isinstance(ref, refs.Reference) else
+                    ref
+                )
+                for ref references
+            )
+        )
+    
+    
+    def __hash__(self):
+        
+        return self.resource.__hash__()
+    
+    
+    def __eq__(self, other):
+        
+        return self.resource == other.resource
+    
+    
+    def __iadd__(self, other):
+        """
+        This will ignore if the other evidence is from different resource:
+        still better than attributing wrong references to a resource.
+        """
+        
+        if self == other:
+            
+            self.references.update(other.references)
+            
+        else:
+            
+            _log(
+                'Warning: attempt to merge evidences from different '
+                'resources. Ignoring the second evidence.'
+            )
+    
+    
+    def merge(self, other):
+        """
+        Merges two evidences. Returns set of either one or two evidences
+        depending on whether the two evidences are from the same resource.
+        """
+        
+        if self == other:
+            
+            self.__iadd__(other)
+            return {self}
+            
+        else:
+            
+            return {self, other}
+
+
+class Evidences(object):
+    
+    
+    def __init__(self, evidences):
+        
+        self.evidences = {}
+        self.__iadd__(evidences)
+    
+    
+    def __iadd__(self, other):
+        
+        other = other if hasattr(other, '__iter__') else (other,)
+        
+        for ev in other:
+            
+            if ev in self.evidences:
+                
+                self.evidences[ev.__hash__()] += ev
+                
+            else:
+                
+                self.evidences[ev.__hash__()] = ev
+    
+    
+    def __iter__(self):
+        
+        for ev in self.evidences.values():
+            
+            yield ev
