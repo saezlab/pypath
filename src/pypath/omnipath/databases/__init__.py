b21ae3e456ebd62d5738574565de01fe6d7d7bb3
Türei Dénes <turei.denes@gmail.com>
2020-02-26 15:35:12 +0100

    database definition module in omnipath.databases

---
+++ src/pypath/omnipath/databases/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module.
+#  Provides a high level interface for managing builds of the
+#  OmniPath databases.
+#
+#  Copyright
+#  2014-2020
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+
+import pypath.omnipath.databases.define as define
+
+
+def builtins(label):
+    """
+    Retrieves the database definition of a built in database.
+    """
+
+    pass
---
+++ src/pypath/omnipath/databases/define.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module.
+#  Provides a high level interface for managing builds of the
+#  OmniPath databases.
+#
+#  Copyright
+#  2014-2020
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+
+
+class DatabaseDefinition(object):
+
+
+    def __init__(self, **kwargs):
+
+        pass
