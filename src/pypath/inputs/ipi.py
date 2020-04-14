eff5570d9a9120a4fcbc917c5060cf2c8b160ec1
Türei Dénes <turei.denes@gmail.com>
2020-04-14 03:21:18 +0200

    inputs.ipi: IPI-UniProt dict

---
+++ src/pypath/inputs/ipi.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2020
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+import collections
+
+import pypath.inputs.common as inputs_common
+import pypath.resources.urls as urls
+import pypath.share.curl as curl
+
+
+def ipi_uniprot():
+    """
+    Retrieves an IPI-UniProt mapping dictionary.
+    """
+    
+    result = collections.defaultdict(set)
+    
+    url = urls.urls['ipi']['url']
+    
+    c = curl.Curl(url, large = True, silent = False)
+    
+    for row in c.result:
+        
+        row = row.strip('\n\r').split('\t')
+        
+        if len(row) < 3:
+            
+            continue
+        
+        ipi_id = row[2]
+        
+        uniprot, isoform = inputs_common._try_isoform(row[1])
+        
+        is_uniprot = (
+            not any(
+                uniprot.startswith(pref)
+                for pref in ('NP_', 'OTTH', 'HIT', 'ENSP', 'XP_')
+            )
+        )
+        
+        if is_uniprot:
+            
+            result[ipi_id].add(uniprot)
+    
+    return dict(result)
