f2355349e48239eb16194254da18d469d69389ce
Türei Dénes <turei.denes@gmail.com>
2020-04-14 20:23:07 +0200

    inputs.embrace: annotations, interactions; + license in resources.json

--- src/pypath/inputs/embrace.py
+++ src/pypath/inputs/embrace.py
@@ -27,8 +27,9 @@ import collections
 
 import pypath.inputs.common as inputs_common
 import pypath.resources.urls as urls
-import pypath.share.curl as curl
 import pypath.utils.mapping as mapping
+import pypath.utils.homology as homology
+import pypath.share.curl as curl
 import pypath.share.common as common
 import pypath.share.session as session
 
@@ -39,7 +40,7 @@ _log = _logger._log
 def embrace_raw():
     """
     Returns Supplementary Table S11 from 10.1016/j.isci.2019.10.026
-    (Sheikh et al. 2019) as a list of lists.
+    (Sheikh et al. 2019) as a list of tuples.
     """
     
     url = urls.urls['embrace']['url']
@@ -141,86 +142,125 @@ def embrace_raw():
     ]
 
 
-def embrace_interactions(organism = 9606):
+def _embrace_id_translation(mouse_genesymbol, organism = 9606):
     
-    
-    EmbraceInteraction = collections.namedtuple(
-        'ItalkInteraction',
-        [
-            'ligand',
-            'receptor',
-            'classification',
-        ]
+    uniprots = mapping.map_name(
+        mouse_genesymbol,
+        'genesymbol',
+        'uniprot',
+        ncbi_tax_id = 10090,
     )
     
+    if organism != 10090:
+        
+        uniprots = homology.translate(
+            uniprots,
+            target = organism,
+            source = 10090,
+        )
     
-    rdata = italk_raw()
+    return uniprots or [None]
+
+
+def embrace_translated(organism = 9606):
+    """
+    Returns Supplementary Table S11 from 10.1016/j.isci.2019.10.026
+    (Sheikh et al. 2019) translated to UniProt IDs of the requested organism.
+    """
+    
+    raw = embrace_raw()
+    record = raw[0].__class__
     result = []
     
-    for row in rdata.itertuples():
+    for row in raw:
         
-        if (
-            not isinstance(row[2], common.basestring) or
-            not isinstance(row[4], common.basestring)
-        ):
-            
-            continue
-        
-        ligands = mapping.map_name(row[2], 'genesymbol', 'uniprot')
-        receptors = mapping.map_name(row[4], 'genesymbol', 'uniprot')
-        cls = row[6]
+        ligands = _embrace_id_translation(
+            row.ligand_symbol,
+            organism = organism,
+        )
+        receptors = _embrace_id_translation(
+            row.receptor_symbol,
+            organism = organism,
+        )
         
         for ligand, receptor in itertools.product(ligands, receptors):
             
             result.append(
-                ItalkInteraction(
-                    ligand = ligand,
-                    receptor = receptor,
-                    classification = cls,
-                )
+                record(*((ligand, receptor) + row[2:]))
             )
     
     return result
 
 
-def italk_annotations():
+def embrace_interactions(organism = 9606):
+    """
+    Returns ligand-receptor interactions from Supplementary Table S11 of
+    10.1016/j.isci.2019.10.026 (Sheikh et al. 2019) translated to UniProt IDs
+    of the requested organism.
+    """
     
     
-    ItalkAnnotation = collections.namedtuple(
-        'ItalkAnnotation',
+    EmbraceInteraction = collections.namedtuple(
+        'EmbraceInteraction',
+        [
+            'ligand',
+            'receptor',
+        ]
+    )
+    
+    return [
+        EmbraceInteraction(*rec[:2])
+        for rec in embrace_translated(organism = organism)
+        if rec[0] and rec[1]
+    ]
+
+
+def embrace_annotations(organism = 9606, ncbi_tax_id = None):
+    """
+    Returns protein annotations from Supplementary Table S11 of
+    10.1016/j.isci.2019.10.026 (Sheikh et al. 2019) translated to UniProt IDs
+    of the requested organism.
+    Returns dict with UniProt IDs as keys and sets of annotation tuples as
+    values.
+    """
+    
+    def _get_value(rec, mainclass, celltype):
+        
+        return bool(getattr(rec, '%s_%s' % (celltype, mainclass)))
+    
+    
+    EmbraceAnnotation = collections.namedtuple(
+        'EmbraceAnnotation',
         [
             'mainclass',
-            'subclass',
+            'neuron',
+            'mural_cell',
+            'microglia',
+            'endothelial_cell',
         ]
     )
     
     
-    rdata = italk_raw()
+    organism = ncbi_tax_id or organism
     result = collections.defaultdict(set)
     
-    for row in rdata.itertuples():
+    for rec in embrace_translated(organism = organism):
         
-        ligands = (
-            mapping.map_name(row[2], 'genesymbol', 'uniprot')
-                if isinstance(row[2], common.basestring) else
-            ()
-        )
-        receptors = (
-            mapping.map_name(row[4], 'genesymbol', 'uniprot')
-                if isinstance(row[4], common.basestring) else
-            ()
-        )
-        subclass = row[6]
-        
-        for mainclass, uniprot in itertools.chain(
-            itertools.zip_longest((), ligands, fillvalue = 'ligand'),
-            itertools.zip_longest((), receptors, fillvalue = 'receptor'),
-        ):
+        for mainclass in ('ligand', 'receptor'):
             
-            result[uniprot].add(
-                ItalkAnnotation(
+            identifier = getattr(rec, '%s_symbol' % mainclass)
+            
+            if not identifier:
+                
+                continue
+            
+            result[identifier].add(
+                EmbraceAnnotation(
                     mainclass = mainclass,
-                    subclass = subclass,
+                    neuron = _get_value(rec, mainclass, 'N'),
+                    mural_cell = _get_value(rec, mainclass, 'P'),
+                    microglia = _get_value(rec, mainclass, 'M'),
+                    endothelial_cell = _get_value(rec, mainclass, 'EC'),
                 )
             )
     
