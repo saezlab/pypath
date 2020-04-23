eb8035bdf40b1ade2c5b196eefcf492f074df1c2
Türei Dénes <turei.denes@gmail.com>
2020-04-23 21:17:37 +0200

    inputs.mcam

---
+++ src/pypath/inputs/mcam.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2020
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+from future.utils import iteritems
+
+import re
+import collections
+
+import pypath.resources.urls as urls
+import pypath.share.curl as curl
+import pypath.utils.mapping as mapping
+
+
+def mcam_cell_adhesion_molecules():
+    
+    url = urls.urls['mcam']['url']
+    
+    c = curl.Curl(url, silent = False, large = True)
+    
+    _ = next(c.result)
+    
+    return {
+        uniprot
+        for line in c.result
+        for _uniprot in line.split('\t', maxsplit = 1)[0].split(';')
+        for uniprot in mapping.map_name(
+            _uniprot.strip(),
+            'uniprot',
+            'uniprot',
+        )
+    }
