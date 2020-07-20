e764b73e9a632a76569cf68e4910b51775f6a110
Türei Dénes <turei.denes@gmail.com>
2020-07-20 21:50:55 +0200

    extended biomart input method

--- src/pypath/inputs/biomart.py
+++ src/pypath/inputs/biomart.py
@@ -22,15 +22,61 @@
 
 import re
 import os
+import collections
 
+import pypath.share.session as session_mod
 import pypath.share.common as common
 import pypath.share.curl as curl
 import pypath.resources.urls as urls
 
+_logger = session_mod.Logger(name = 'biomart_input')
 
-def biomart_query(attr, transcript = False):
 
-    # attr = 'hgnc_symbol'
+# for mouse homologues: Filter name = "with_mmusculus_homolog"
+_filter_xml_template = '<Filter name="%s" excluded="0"/>'
+_attr_xml_template = '<Attribute name="%s" />'
+
+
+def biomart_query(
+        attrs,
+        filters = None,
+        transcript = False,
+        gene = False,
+        dataset = 'hsapiens_gene_ensembl',
+    ):
+
+    _attrs = []
+
+    if gene:
+
+        _attrs.append('ensembl_gene_id')
+
+    if transcript:
+
+        _attrs.append('ensembl_transcript_id')
+
+    _attrs.extend(common.to_list(attrs))
+    filters = common.to_list(filters)
+
+    record = collections.namedtuple(
+        'EnsemblRecord',
+        _attrs
+    )
+
+    _logger._log(
+        'Downloading data from Ensembl Biomart: '
+        'dataset=`%s`, '
+        '%s'
+        'attributes=`%s`.' % (
+            dataset,
+            (
+                'filters=`%s`, ' % ', '.join(filters)
+                    if filters else
+                ''
+            ),
+            ', '.join(_attrs),
+        )
+    )
 
     rewsp = re.compile(r'\n\s+')
 
@@ -40,11 +86,19 @@ def biomart_query(attr, transcript = False):
 
         xml_template = fp.read()
 
-    ens_id_type = 'transcript' if transcript else 'gene'
+    filter_part = ''.join(
+        _filter_xml_template % _filter
+        for _filter in filters
+    )
+    attr_part = ''.join(
+        _attr_xml_template % _attr
+        for _attr in _attrs
+    )
 
     xml_query = xml_template % (
-        ens_id_type,
-        attr,
+        dataset,
+        filter_part,
+        attr_part,
     )
     xml_query = rewsp.sub('', xml_query)
 
@@ -52,10 +106,21 @@ def biomart_query(attr, transcript = False):
 
     c = curl.Curl(biomart_url, large = True, silent = False)
 
+    success = False
+
     for line in c.result:
 
         line = line.strip('\n\r').split('\t')
 
-        if len(line) >= 2:
+        success = success or line[0] == '[success]'
 
-            yield line
+        if len(line) == len(record._fields):
+
+            yield record(*line)
+
+    if not success:
+
+        _logger._log(
+            'Error: Interrupted transfer while downlading data '
+            'from Ensembl Biomart (missing `success` tag).'
+        )
