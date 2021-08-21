3a54eba9c85214d298c787c09d97b78887a09138
Türei Dénes <turei.denes@gmail.com>
2021-08-21 17:18:05 +0200

    missing modules in inputs (mirecords, wang)

---
+++ src/pypath/inputs/mirecords.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2021
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+import collections
+
+import pypath.resources.urls as urls
+import pypath.share.curl as curl
+import pypath.inputs.common as inputs_common
+
+
+def mirecords_interactions():
+    """
+    Retrieves literature curated miRNA-target gene interactions from
+    miRecords (c1.accurascience.com/miRecords/).
+    """
+
+    MirecordsInteraction = collections.namedtuple(
+        'MirecordsInteraction',
+        (
+            'mirna_name',
+            'target_refseq',
+            'target_genesymbol',
+            'mirna_organism',
+            'target_organism',
+            'pmid',
+        ),
+    )
+
+    url = urls.urls['mirecords']['url']
+    c = curl.Curl(url, silent = False, large = True)
+
+    tbl = inputs_common.read_xls(c.fileobj.name)
+
+    c.close()
+
+    return [
+        MirecordsInteraction(
+            mirna_name = l[6],
+            target_refseq = l[3],
+            target_genesymbol = l[2],
+            mirna_organism = l[1],
+            target_organism = l[5],
+            pmid = l[0].split('.')[0],
+        )
+        for l in
+        ([f.strip() for f in ll] for ll in tbl[1:])
+    ]
---
+++ src/pypath/inputs/wang.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2021
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#                  Nicolàs Palacio
+#                  Olga Ivanova
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+import pypath.resources.urls as urls
+import pypath.share.curl as curl
+
+
+def get_hsn():
+    """
+    Downloads and processes HumanSignalingNetwork version 6
+    (published 2014 Jan by Edwin Wang).
+    Returns list of interactions.
+    """
+
+    url = urls.urls['hsn']['url']
+    c = curl.Curl(url, silent = False, large = True)
+    data = c.result
+    data = [r.strip().split(',') for r in data if r][1:]
+
+    return data
+
+
+def wang_interactions():
+    """
+    Downloads and processes Wang Lab HumanSignalingNetwork.
+    Returns list of interactions as tuples of source, target and effect.
+    """
+
+    url = urls.urls['wang']['rescued']
+    c = curl.Curl(url, silent = False)
+    data = c.result
+    data = data.split('\n')
+    effects = []
+    nodes = {}
+    reading_nodes = False
+    reading_edges = False
+
+    for l in data:
+
+        if len(l.strip()) == 0:
+            reading_nodes = False
+            reading_edges = False
+
+        l = l.split(',')
+
+        if reading_nodes:
+            nodes[l[0]] = l[1]
+
+        if reading_edges:
+            effects.append([nodes[l[0]], nodes[l[1]], l[2]])
+
+        if l[0].startswith('Node'):
+            reading_nodes = True
+
+        if l[0].startswith('From'):
+            reading_nodes = False
+            reading_edges = True
+
+    return effects
