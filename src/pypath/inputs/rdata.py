d3d7e9d0602a6b11496243c390f38edb9552714e
Türei Dénes <turei.denes@gmail.com>
2021-11-03 19:37:08 +0100

    `rdata`: fixed rowname extraction (have no idea how it got broken :D )

--- src/pypath/inputs/rdata.py
+++ src/pypath/inputs/rdata.py
@@ -25,33 +25,35 @@ import numpy as np
 
 def _rdata_data_frame_get_rownames(robj):
 
-    for i, attr in enumerate(robj.attributes):
+    for i, attr in enumerate(robj.attributes.value):
 
         if (
-            attr and
-            attr[1] and (
+            attr.value and
+            attr.value[1] and (
                 (
-                    hasattr(attr[1].value[1], 'tag') and
-                    attr[1].value[1].tag and (
+                    hasattr(attr.value[1], 'tag') and
+                    attr.value[1].tag and (
                         (
-                            attr[1].value[1].tag.referenced_object and
-                            attr[1].value[1].tag.referenced_object.value and
-                            attr[1].value[1].tag.referenced_object.value.\
+                            attr.value[1].tag.referenced_object and
+                            attr.value[1].tag.referenced_object.value and
+                            attr.value[1].tag.referenced_object.value.\
                                 value == b'row.names'
                         ) or (
-                            attr[1].value[1].tag.value and
-                            attr[1].value[1].tag.value.value == b'row.names'
+                            attr.value[1].tag.value and
+                            attr.value[1].tag.value.value == b'row.names'
                         )
                     )
                 ) or (
-                    attr[1].tag and
-                    attr[1].tag.referenced_object and
-                    attr[1].tag.referenced_object.value and
-                    attr[1].tag.referenced_object.value.value == b'row.names'
+                    attr.value[1].tag and
+                    attr.value[1].tag.referenced_object and
+                    attr.value[1].tag.referenced_object.value and (
+                        attr.value[1].tag.referenced_object.value.value ==
+                        b'row.names'
+                    )
                 ) or (
-                    attr[1].tag and
-                    attr[1].tag.value and
-                    attr[1].tag.value.value == b'row.names'
+                    attr.value[1].tag and
+                    attr.value[1].tag.value and
+                    attr.value[1].tag.value.value == b'row.names'
                 )
             )
         ):
@@ -59,11 +61,11 @@ def _rdata_data_frame_get_rownames(robj):
             break
 
     rownames = (
-        attr[1].value[1].value[0].value
-            if attr[1].value[0].value[0].value == b'data.frame' else
-        attr[1].value[0].value
+        attr.value[1].value[0].value
+            if attr.value[0].value[0].value == b'data.frame' else
+        attr.value[0].value
             if (
-                attr[1].value[1].value[0].value[0].value ==
+                attr.value[1].value[0].value[0].value ==
                 b'data.frame'
             ) else
         []
