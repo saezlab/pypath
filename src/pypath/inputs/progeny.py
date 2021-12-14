15be37c571f3b217f293a376309e02e9b35600ef
Türei Dénes <turei.denes@gmail.com>
2021-12-14 15:27:49 +0100

    PROGENy input in annotation format

--- src/pypath/inputs/progeny.py
+++ src/pypath/inputs/progeny.py
@@ -27,6 +27,7 @@ import pypath.share.session as session
 import pypath.share.curl as curl
 import pypath.resources.urls as urls
 import pypath.utils.taxonomy as taxonomy
+import pypath.utils.mapping as mapping
 import pypath.inputs.rdata as rdata
 
 _logger = session.Logger(name = 'progeny_input')
@@ -35,9 +36,16 @@ _log = _logger._log
 
 def progeny_raw(organism = 9606):
     """
+    Pathway responsive genes: signatures based on transcriptomics data
+    from PROGENy (https://github.com/saezlab/progeny).
+
     Args:
         organism (int,str): Name or NCBI Taxonomy ID of the organism. Human
             and mouse are supported.
+
+    Returns:
+        (pandas.DataFrame): A data frame of genes, pathways, weights and
+            p-values for each association.
     """
 
     _organism = taxonomy.ensure_common_name(organism)
@@ -65,3 +73,52 @@ def progeny_raw(organism = 9606):
     key = 'model_%s_full' % _organism
 
     return rdata_converted[key]
+
+
+def progeny_annotations(organism = 9606):
+    """
+    Pathway responsive genes: signatures based on transcriptomics data
+    from PROGENy (https://github.com/saezlab/progeny).
+
+    Args:
+        organism (int,str): Name or NCBI Taxonomy ID of the organism. Human
+            and mouse are supported.
+
+    Returns:
+        (dict): Dict of sets, keys are
+    """
+
+    record = collections.namedtuple(
+        'ProgenyAnnotation',
+        (
+            'pathway',
+            'weight',
+            'p_value',
+        )
+    )
+
+    raw = progeny_raw(organism = organism)
+    result = collections.defaultdict(set)
+
+    ncbi_tax_id = taxonomy.ensure_ncbi_tax_id(organism)
+
+    for rec in raw.itertuples():
+
+        uniprots = mapping.map_name(
+            rec.gene,
+            'genesymbol',
+            'uniprot',
+            ncbi_tax_id = ncbi_tax_id,
+        )
+
+        annot = record(
+            pathway = rec.pathway,
+            weight = rec.weight,
+            p_value = rec[4], # omg, stupid pandas
+        )
+
+        for uniprot in uniprots:
+
+            result[uniprot].add(annot)
+
+    return dict(result)
