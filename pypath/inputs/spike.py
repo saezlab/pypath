061b8c595fb687ffcee3f44f67e6a2f1c3b03624
deeenes <turei.denes@gmail.com>
2022-04-16 03:46:42 +0200

    SPIKE: processing complexes

--- pypath/inputs/spike.py
+++ pypath/inputs/spike.py
@@ -22,10 +22,15 @@
 #  Website: http://pypath.omnipathdb.org/
 #
 
+import itertools
+import collections
+
 import xml.etree.cElementTree as ET
 
 import pypath.share.curl as curl
 import pypath.resources.urls as urls
+import pypath.utils.mapping as mapping
+import pypath.internals.intera as intera
 
 
 def spike_interactions(high_confidence = True):
@@ -52,97 +57,119 @@ def spike_interactions(high_confidence = True):
     genes = {}
     result = []
 
+    SpikeInteraction = collections.namedtuple(
+        'SpikeInteraction',
+        (
+            'entrez_a',
+            'genesymbol_a',
+            'entrez_b',
+            'genesymbol_b',
+            'directed',
+            'pmids',
+            'integrity',
+            'effect',
+            'assay',
+            'data_source',
+            'description',
+            'mechanism',
+            'regulation',
+        )
+    )
+
+    Gene = collections.namedtuple('Gene', ('entrez', 'genesymbol', 'type'))
+
     for gene in bblock.findall('Gene'):
 
         sy = '' if 'name' not in gene.attrib else gene.attrib['name']
-        genes[gene.attrib['id']] = (gene.find('XRef').attrib['id'], sy)
+        genes[gene.attrib['id']] = [
+            Gene(
+                entrez = gene.find('XRef').attrib['id'],
+                genesymbol = sy,
+                type = 'gene',
+            )
+        ]
 
-    for reg in rblock.findall('Regulation'):
+    for grp in bblock.findall('Group'):
 
-        ds = reg.attrib['dataSource']
-        itg = reg.attrib['integrity']
-        eff = reg.attrib['effect']
-        mec = reg.attrib['mechanism']
-        src = reg.find('Source').attrib['ref']
-        tgt = reg.find('PhysicalTarget').attrib['ref']
-        dcd = "1"
+        if grp.attrib['type'] == 'Complex':
+
+            members = [m.attrib['ref'] for m in grp.findall('Member')]
+
+            if all(
+                m in genes and
+                genes[m][0].type != 'complex'
+                for m in members
+            ):
+
+                uniprots = [
+                    mapping.map_name(genes[m][0].entrez, 'entrez', 'uniprot')
+                    for m in members
+                ]
+
+                genes[grp.attrib['id']] = [
+                    Gene(
+                        intera.Complex(
+                            name = grp.attrib['name'],
+                            components = ups,
+                            sources = 'SPIKE',
+                        ),
+                        None,
+                        'complex',
+                    )
+                    for ups in itertools.product(*uniprots)
+                ]
+
+    for i in itertools.chain(
+            rblock.findall('Regulation'),
+            iblock.findall('Interaction'),
+        ):
+
+        regulation = i.tag == 'Regulation'
+        src_tag = 'Source' if regulation else 'ProteinA'
+        tgt_tag = 'PhysicalTarget' if regulation else 'ProteinB'
+
+        ds = i.attrib['dataSource']
+        itg = i.attrib['integrity']
+        eff = i.attrib.get('effect', '')
+        mec = i.attrib.get('mechanism', '')
+        src = i.find(src_tag).attrib['ref']
+        tgt = i.find(tgt_tag).attrib['ref']
+        dcd = str(int(regulation))
         dsc = (
             ''
-                if reg.find('Description') is None else
-            reg.find('Description').text.replace('\n', ' ')
+                if i.find('Description') is None else
+            i.find('Description').text.replace('\n', ' ')
         )
         asy = (
             ''
-                if 'biologicalAssay' not in reg.attrib else
-            reg.attrib['biologicalAssay']
+                if 'biologicalAssay' not in i.attrib else
+            i.attrib['biologicalAssay']
         )
-        refs = reg.findall('Reference')
-        pmids = []
-
-        for r in refs:
-            pmids.append(r.attrib['pmid'])
+        refs = i.findall('Reference')
+        pmids = [r.attrib['pmid'] for r in refs]
 
         if src in genes and tgt in genes:
 
             if itg == '1' or not high_confidence:
 
-                result.append([
-                    genes[src][0],
-                    genes[src][1],
-                    genes[tgt][0],
-                    genes[tgt][1],
-                    dcd,
-                    ';'.join(pmids),
-                    itg,
-                    eff,
-                    asy,
-                    ds,
-                    dsc,
-                    mec,
-                ])
+                for _src, _tgt in itertools.product(genes[src], genes[tgt]):
 
-    for ict in iblock.findall('Interaction'):
-
-        ds = ict.attrib['dataSource']
-        itg = ict.attrib['integrity']
-        eff = '' if 'effect' not in ict.attrib else ict.attrib['effect']
-        src = ict.find('ProteinA').attrib['ref']
-        tgt = ict.find('ProteinB').attrib['ref']
-        dcd = "0"
-        mec = ""
-        dsc = (
-            ''
-                if ict.find('Description') is None else
-            ict.find('Description').text.replace('\n', ' ')
-        )
-        asy = (
-            ''
-                if 'biologicalAssay' not in ict.attrib else
-            ict.attrib['biologicalAssay']
-        )
-        refs = ict.findall('Reference')
-        pmids = []
-
-        for r in refs:
-            pmids.append(r.attrib['pmid'])
-
-        if src in genes and tgt in genes:
-
-            if itg == '1' or not high_confidence:
-
-                result.append([
-                    genes[src][0],
-                    genes[src][1],
-                    genes[tgt][0],
-                    genes[tgt][1],
-                    dcd,
-                    ';'.join(pmids),
-                    itg,
-                    eff,
-                    asy,
-                    ds,
-                    dsc,
-                    mec,
-                ])
+                    result.append(
+                        SpikeInteraction(
+                            entrez_a = _src.entrez,
+                            genesymbol_a = _src.genesymbol,
+                            entrez_b = _tgt.entrez,
+                            genesymbol_b = _tgt.genesymbol,
+                            directed = dcd,
+                            pmids = ';'.join(pmids),
+                            integrity = itg,
+                            effect = eff,
+                            assay = asy,
+                            data_source = ds,
+                            description = dsc,
+                            mechanism = mec,
+                            regulation = regulation,
+                        )
+                    )
 
     return result
