996fa6eeb5ade4a9c951150812ecd9623eeb4466
deeenes <turei.denes@gmail.com>
2022-05-05 17:14:10 +0200

    Lambert 2018 TF annotations

--- pypath/inputs/lambert2018.py
+++ pypath/inputs/lambert2018.py
@@ -63,3 +63,63 @@ def lambert2018_s1_raw():
         record(*(common.try_bool(common.try_float(f)) for f in r[:nfields]))
         for r in content
     ]
+
+
+def lambert2018_annotations():
+
+    Lambert2018Annotation = collections.namedtuple(
+        'Lambert2018Annotation',
+        (
+            'ensg',
+            'genesymbol',
+            'is_tf',
+            'tf_assessment',
+            'binding_mode',
+            'binding_domain',
+            'tf_disagree',
+            'binding_disagree',
+            'binding1',
+            'binding2',
+            'assessment1',
+            'assessment2',
+            'vaquerizas2009',
+            'cisbp',
+            'tfclass',
+            'tfcat',
+            'go',
+            'pdb',
+        )
+    )
+
+    result = collections.defaultdict(set)
+
+    for r in lambert2018_s1_raw():
+
+        uniprots = mapping.map_name(r.name, 'genesymbol', 'uniprot')
+
+        for uniprot in uniprots:
+
+            result[uniprot].add(
+                Lambert2018Annotation(
+                    ensg = r.id,
+                    genesymbol = r.name,
+                    is_tf = r.is_tf,
+                    tf_assessment = r.tf_assessment,
+                    binding_mode = r.binding_mode,
+                    binding_domain = r.dbd,
+                    tf_disagree = r.disagree_on_assessment,
+                    binding_disagree = r.disagree_on_binding,
+                    binding1 = r.binding1,
+                    binding2 = r.binding2,
+                    assessment1 = r.assesment1,
+                    assessment2 = r.assesment2,
+                    vaquerizas2009 = r.vaquerizas_2009_tf_classification,
+                    cisbp = r.cisbp_considers_it_as_a_tf,
+                    tfclass = r.tfclass_considers_it_as_a_tf,
+                    tfcat = r.tf_cat_classification,
+                    go = r.is_a_go_tf,
+                    pdb = r.pdb,
+                )
+            )
+
+    return dict(result)
