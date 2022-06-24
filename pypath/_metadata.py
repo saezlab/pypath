5c401f11405fe6c573045dfb620a96fc019b140a
deeenes <turei.denes@gmail.com>
2022-06-24 02:23:11 +0200

    single ultimate source of version data is `pyproject.toml`, `_version` is replaced by `_metadata`

---
+++ pypath/_metadata.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2022
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: Dénes Türei (turei.denes@gmail.com)
+#           Nicolàs Palacio
+#           Sebastian Lobentanzer
+#           Erva Ulusoy
+#           Olga Ivanova
+#           Ahmet Rifaioglu
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+"""
+Package metadata (version, authors, etc).
+"""
+
+__all__ = ['get_metadata']
+
+import os
+import pathlib
+import importlib.metadata
+
+import toml
+
+
+def get_metadata():
+    """
+    Basic package metadata.
+
+    Retrieves package metadata from the current project directory or from
+    the installed package.
+    """
+
+    here = pathlib.Path(__file__).parent
+    pyproj_toml = 'pyproject.toml'
+    meta = {}
+
+    for project_dir in (here, here.parent):
+
+        toml_path = str(project_dir.joinpath(pyproj_toml).absolute())
+
+        if os.path.exists(toml_path):
+
+            pyproject = toml.load(toml_path)
+
+            meta = {
+                'name': pyproject['tool']['poetry']['name'],
+                'version': pyproject['tool']['poetry']['version'],
+                'author': pyproject['tool']['poetry']['authors'],
+                'license': pyproject['tool']['poetry']['license'],
+                'full_metadata': pyproject,
+            }
+
+            break
+
+    if not meta:
+
+        meta = dict(importlib.metadata.metadata(here.name).items())
+
+    return meta
+
+
+metadata = get_metadata()
+__version__ = metadata.get('version', None)
+__author__ = metadata.get('author', None)
+__license__ = metadata.get('license', None)
