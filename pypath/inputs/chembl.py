a65b301b428ac5b0235708daa11870ea3481fe4f
Tennur Kılıç <99681146+tnnrklc@users.noreply.github.com>
2022-08-12 00:10:13 +0300

    Add files via upload

---
+++ pypath/inputs/chembl.py
@@ -0,0 +1,278 @@
+from typing import List
+
+import json
+import collections
+
+import pypath.share.curl as curl
+import pypath.resources.urls as urls
+
+def chembl_targets() -> List[tuple] :
+    """
+    Retrieves targets data from ChEMBL.
+    
+    Returns: 
+        namedtuple.
+    """
+
+    fields_target = ('accession','target_chembl_id')
+
+    Target = collections.namedtuple('Target', fields_target,defaults = ("None",) * len(fields_target))
+
+    trgtlst = []
+
+    flag = 0
+
+    while True:
+
+        if flag == 0:
+
+            url = urls.urls['chembl']['url'] + urls.urls['chembl']['target']
+            c = curl.Curl(url, large=True, silent=False)
+            flag = 1
+
+        else:
+
+            if lst['page_meta']['next']:
+
+                url = urls.urls['chembl']['url'] + lst['page_meta']['next']
+                c = curl.Curl(url, large=True, silent=False)
+
+            else:
+
+                break
+        
+        fileObject = open(c.fileobj.name)
+        lst = json.loads(fileObject.read())
+        
+        for trgt_attr in lst['targets']:
+
+            if trgt_attr['target_components']:
+
+                trgtlst.append(
+                    Target(
+                        accession = trgt_attr['target_components'][0]['accession'],
+                        target_chembl_id = trgt_attr['target_chembl_id'],
+                        )
+                    )
+
+            else:
+                
+                trgtlst.append(
+                    Target(
+                        target_chembl_id = trgt_attr['target_chembl_id'],
+                        )
+                    )
+
+    return trgtlst
+
+def chembl_assays() -> List[tuple] :
+    """
+    Retrieves assays data from ChEMBL.
+    
+    Returns: 
+        namedtuple.
+    """
+
+    fields_assay = ('assay_chembl_id','assay_organism','assay_type','confidence_score','target_chembl_id')
+
+    Assay = collections.namedtuple('Assay', fields_assay,defaults = ("None",) * len(fields_assay))
+
+    assylst = []
+
+    flag = 0
+
+    while True:
+
+        if flag == 0:
+
+            url = urls.urls['chembl']['url'] + urls.urls['chembl']['assay']
+            c = curl.Curl(url, large=True, silent=False)
+            flag = 1
+
+        else:
+
+            if lst['page_meta']['next']:
+
+                url = urls.urls['chembl']['url'] + lst['page_meta']['next']
+                c = curl.Curl(url, large=True, silent=False)
+
+            else:
+
+                break
+        
+        fileObject = open(c.fileobj.name)
+        lst = json.loads(fileObject.read())
+        
+        for assy_attr in lst['assays']:
+            
+            assylst.append(
+                Assay(
+                    assay_chembl_id = assy_attr['assay_chembl_id'],
+                    assay_organism = assy_attr['assay_organism'],
+                    assay_type = assy_attr['assay_type'],
+                    confidence_score = assy_attr['confidence_score'],
+                    target_chembl_id = assy_attr['target_chembl_id'],
+                    )
+                )
+
+    return assylst
+
+def chembl_molecules() -> List[tuple] :
+    """
+    Retrieves molecules data from ChEMBL.
+    
+    Returns: 
+        namedtuple.
+    """
+
+    fields_molecule = ('alogp','conanicle_smiles','chirality','full_mwt','heavy_atoms','standard_inchi_key','molecular_species',
+                        'molecul_type','molecule_chembl_id','parent_chembl_id','prodrug','standard_inchi', 'xrefs')
+
+    Molecule = collections.namedtuple('Molecule', fields_molecule,defaults = ("None",) * len(fields_molecule))
+
+    mlcllst = []
+
+    flag = 0
+
+    while True:
+
+        if flag == 0:
+
+            url = urls.urls['chembl']['url'] + urls.urls['chembl']['molecule']
+            c = curl.Curl(url, large=True, silent=False)
+            flag = 1
+
+        else:
+
+            if lst['page_meta']['next']:
+
+                url = urls.urls['chembl']['url'] + lst['page_meta']['next']
+                c = curl.Curl(url, large=True, silent=False)
+
+            else:
+
+                break
+        
+        fileObject = open(c.fileobj.name)
+        lst = json.loads(fileObject.read())
+        
+        for mlcl_attr in lst['molecules']:
+
+            xrefs = []
+            mlcllst.append(
+                Molecule(
+                    chirality = mlcl_attr['chirality'],
+                    molecul_type = mlcl_attr['molecule_type'],
+                    prodrug = mlcl_attr['prodrug'],
+                    )
+                )
+                
+            if mlcl_attr['molecule_hierarchy'] != None:
+                mlcllst[-1] = mlcllst[-1]._replace(
+                    molecule_chembl_id = mlcl_attr['molecule_hierarchy']['molecule_chembl_id'],
+                    parent_chembl_id = mlcl_attr['molecule_hierarchy']['parent_chembl_id'],
+                )
+
+            if mlcl_attr['molecule_properties'] != None:
+                mlcllst[-1] = mlcllst[-1]._replace(
+                    alogp = mlcl_attr['molecule_properties']['alogp'],
+                    full_mwt = mlcl_attr['molecule_properties']['full_mwt'],
+                    heavy_atoms = mlcl_attr['molecule_properties']['heavy_atoms'],
+                    molecular_species = mlcl_attr['molecule_properties']['molecular_species'],
+                )   
+            
+            if mlcl_attr['molecule_structures'] != None:
+                mlcllst[-1] = mlcllst[-1]._replace(
+                    conanicle_smiles = mlcl_attr['molecule_structures']['canonical_smiles'],
+                    standard_inchi_key = mlcl_attr['molecule_structures']['standard_inchi_key'],
+                    standard_inchi = mlcl_attr['molecule_structures']['standard_inchi'],
+                )
+            
+            if mlcl_attr['cross_references'] != None:
+
+                for rec in mlcl_attr['cross_references']:
+                    
+                    xrefs.append({'xref_id' : rec['xref_id'], 'xref_src': rec['xref_src']})
+
+                mlcllst[-1] = mlcllst[-1]._replace(
+                    xrefs = xrefs
+                )
+
+                
+    return mlcllst
+
+def chembl_activities(
+        pchembl_value_none: bool = False,
+        standard_relation: bool = '=',
+    ) -> List[tuple] :
+    """
+    Retrieves activities data from ChEMBL.
+
+    Args:
+        pchembl_value_none (bool): Whether the pchembl value should be none or not.
+        standard_relation (str): Which standard relation in needed.
+
+    Returns: 
+        namedtuple.
+            standard_flag and standard_units attributes are not included in the returned namedtuple.
+            Only records returned are the ones where data_validity_comment is none.
+    """
+
+    fields_activity = ('assay_chembl_id','data_validity_comment','molecule_chembl_id','pchembl_value',
+                        'standard_relation','standard_value','target_chembl_id')
+
+    Activity = collections.namedtuple('Activity', fields_activity,defaults = ("None",) * len(fields_activity))
+
+    actvtylst = []
+
+    flag = 0
+
+    while True:
+
+        if flag == 0:
+
+            if pchembl_value_none == True:
+                
+                url = urls.urls['chembl']['url'] + urls.urls['chembl']['activity']+'&pchembl_value__isnull=true'
+                
+            else:
+                
+                url = urls.urls['chembl']['url'] + urls.urls['chembl']['activity']+'&pchembl_value__isnull=false'
+
+            url = url + '&standard_relation__exact='+standard_relation
+            c = curl.Curl(url, large=True, silent=False)
+            flag = 1
+
+        else:
+
+            if lst['page_meta']['next']:
+
+                url = urls.urls['chembl']['url'] + lst['page_meta']['next']
+                c = curl.Curl(url, large=True, silent=False)
+
+            else:
+
+                break
+        
+        fileObject = open(c.fileobj.name)
+        lst = json.loads(fileObject.read())
+
+        
+        for actvty_attr in lst['activities']:
+
+            if actvty_attr['data_validity_comment'] == None:
+                
+                actvtylst.append(
+                    Activity(
+                        assay_chembl_id = actvty_attr['assay_chembl_id'],
+                        data_validity_comment = actvty_attr['data_validity_comment'],
+                        molecule_chembl_id = actvty_attr['molecule_chembl_id'],
+                        pchembl_value = actvty_attr['pchembl_value'],
+                        standard_relation = actvty_attr['standard_relation'],
+                        standard_value = actvty_attr['standard_value'],
+                        target_chembl_id = actvty_attr['target_chembl_id'],
+                        )
+                    )  
+
+                
+    return actvtylst
---
+++ pypath/inputs/drugbank.py
@@ -0,0 +1,418 @@
+from typing import List
+
+import os
+import csv
+import collections
+import base64
+
+import pypath.resources.urls as urls
+import pypath.share.curl as curl
+import pypath.share.session as session
+import pypath.share.settings as settings
+
+_logger = session.Logger(name = 'drugbank')
+_log = _logger._log
+
+def add_prot_id(
+        user: str, 
+        passwd: str, 
+        pharma_active: bool = False,
+    ) -> List[tuple] :
+    """
+    Retrieves protein identifiers from Drugbank.
+
+    Args:
+        user (str): E-mail address for login to DrugBank.
+        passwd (str): Password for login to DrugBank.
+        pharma_active (bool): Wheter to include pharmacologically active identifiers.
+
+    Returns:
+        namedtuple.
+    """
+
+    credentials = {'user': user, 'passwd': passwd}
+
+    auth_str = base64.b64encode(
+        ('%s:%s' % (credentials['user'], credentials['passwd'])).encode()
+    ).decode()
+
+    decoded = 'Basic %s' % auth_str
+
+    req_hdrs = ['Authorization: %s' % decoded]
+    req_hdrs.extend([settings.get('user_agent')])
+
+    fields = ('DrugBank_ID','Target_UniProt_ID','Transporter_UniProt_ID','Enzym_UniProt_ID','Carrier_UniProt_ID')
+
+    ProteinIdentifiers = collections.namedtuple('ProteinIndetifiers', fields,defaults = ("",) * len(fields))
+
+    url = urls.urls['drugbank']['drug_enzym_identifiers']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False,
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".csv.zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".csv.zip")
+    enzym = list(csv.DictReader(zipfile.result["all.csv"], delimiter = ','))
+
+    if pharma_active:
+
+        active = list(csv.DictReader(zipfile.result["pharmacologically_active.csv"], delimiter = ','))
+
+        for rec in active:
+
+            enzym.append(rec)
+    
+    result = []
+
+    result.append(
+        ProteinIdentifiers(
+            DrugBank_ID = "",
+            )
+        )
+
+    for enzym_attr in enzym:
+
+        DrugBank_IDs = [i for i in enzym_attr['Drug IDs'].replace(" ","").split(';')]
+
+        for id in DrugBank_IDs:
+
+            index = 0
+            flag = 0
+
+            for res_attr in result:
+
+                if id == res_attr.DrugBank_ID:
+
+                    flag = 1
+
+                    if res_attr.Enzym_UniProt_ID == "":
+
+                        result[index] = result[index]._replace(
+                        Enzym_UniProt_ID = enzym_attr['UniProt ID'],)
+
+                    else:
+
+                        result[index] = result[index]._replace(
+                        Enzym_UniProt_ID = result[index].Enzym_UniProt_ID + ";" + enzym_attr['UniProt ID'],)
+
+                    break
+
+                index += 1
+
+            if flag == 0:
+
+                result.append(
+                    ProteinIdentifiers(
+                        DrugBank_ID = id,
+                        Enzym_UniProt_ID = enzym_attr['UniProt ID'],
+                        )
+                    )
+
+    del result[0]
+
+    url = urls.urls['drugbank']['drug_carrier_identifiers']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False,
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".csv.zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".csv.zip")
+    carrier = list(csv.DictReader(zipfile.result["all.csv"], delimiter = ','))
+
+    if pharma_active:
+
+        active = list(csv.DictReader(zipfile.result["pharmacologically_active.csv"], delimiter = ','))
+
+        for rec in active:
+
+            carrier.append(rec)
+
+    for carrier_attr in carrier:
+
+        DrugBank_IDs = [i for i in carrier_attr['Drug IDs'].replace(" ","").split(';')]
+
+        for id in DrugBank_IDs:
+
+            index = 0
+            flag = 0
+
+            for res_attr in result:
+
+                if id == res_attr.DrugBank_ID:
+
+                    flag = 1
+
+                    if res_attr.Carrier_UniProt_ID == "":
+
+                        result[index] = result[index]._replace(
+                        Carrier_UniProt_ID = carrier_attr['UniProt ID'],)
+
+                    else:
+
+                        result[index] = result[index]._replace(
+                        Carrier_UniProt_ID = result[index].Carrier_UniProt_ID + ";" + carrier_attr['UniProt ID'],)
+
+                    break
+
+                index += 1
+
+            if flag == 0:
+
+                result.append(
+                    ProteinIdentifiers(
+                        DrugBank_ID = id,
+                        Carrier_UniProt_ID = carrier_attr['UniProt ID'],
+                        )
+                    )
+
+
+    url = urls.urls['drugbank']['drug_transporter_identifiers']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False,
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".csv.zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".csv.zip")
+    transporter = list(csv.DictReader(zipfile.result["all.csv"], delimiter = ','))
+
+    if pharma_active:
+
+        active = list(csv.DictReader(zipfile.result["pharmacologically_active.csv"], delimiter = ','))
+
+        for rec in active:
+
+            transporter.append(rec)
+
+    for transporter_attr in transporter:
+
+        DrugBank_IDs = [i for i in transporter_attr['Drug IDs'].replace(" ","").split(';')]
+
+        for id in DrugBank_IDs:
+
+            index = 0
+            flag = 0
+
+            for res_attr in result:
+
+                if id == res_attr.DrugBank_ID:
+
+                    flag = 1
+
+                    if res_attr.Transporter_UniProt_ID == "":
+
+                        result[index] = result[index]._replace(
+                        Transporter_UniProt_ID = transporter_attr['UniProt ID'],)
+
+                    else:
+
+                        result[index] = result[index]._replace(
+                        Transporter_UniProt_ID = result[index].Transporter_UniProt_ID + ";" + transporter_attr['UniProt ID'],)
+
+                    break
+
+                index += 1
+
+            if flag == 0:
+
+                result.append(
+                    ProteinIdentifiers(
+                        DrugBank_ID = id,
+                        Transporter_UniProt_ID = transporter_attr['UniProt ID'],
+                        )
+                    )
+
+    url = urls.urls['drugbank']['drug_target_identifiers']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False,
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".csv.zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".csv.zip")
+    target = list(csv.DictReader(zipfile.result["all.csv"], delimiter = ','))
+
+    if pharma_active:
+
+        active = list(csv.DictReader(zipfile.result["pharmacologically_active.csv"], delimiter = ','))
+
+        for rec in active:
+
+            target.append(rec)
+
+    for target_attr in target:
+
+        DrugBank_IDs = [i for i in target_attr['Drug IDs'].replace(" ","").split(';')]
+
+        for id in DrugBank_IDs:
+
+            index = 0
+            flag = 0
+
+            for res_attr in result:
+
+                if id == res_attr.DrugBank_ID:
+
+                    flag = 1
+
+                    if res_attr.Target_UniProt_ID == "":
+
+                        result[index] = result[index]._replace(
+                        Target_UniProt_ID = target_attr['UniProt ID'],)
+
+                    else:
+
+                        result[index] = result[index]._replace(
+                        Target_UniProt_ID = result[index].Target_UniProt_ID + ";" + target_attr['UniProt ID'],)
+
+                    break
+
+                index += 1
+                
+            if flag == 0:
+
+                result.append(
+                    ProteinIdentifiers(
+                        DrugBank_ID = id,
+                        Target_UniProt_ID = target_attr['UniProt ID'],
+                        )
+                    )
+
+    return result
+
+def drug_bank(
+        user: str, 
+        passwd: str, 
+        addprotid: bool = True, 
+        pharma_active: bool = False,
+    ) -> List[tuple] :
+    """
+    Retrieves structures, external links and protein identifiers from Drugbank.
+
+    Args:
+        user (str): E-mail address for login to DrugBank.
+        passwd (str): Password for login to DrugBank.
+        addprotid (bool): Wheter to include protein identifiers from DrugBank.
+        pharma_active (bool): Wheter to include pharmacologically active identifiers.
+
+    Returns:
+        namedtuple.
+    """
+
+    fields = ('DrugBank_ID','Name','CAS_Number','Drug_Groups','InChIKey','InChI','SMILES','Formula',
+                'KEGG_Compound_ID','KEGG_Drug_ID','PubChem_Compound_ID','PubChem_Substance_ID','ChEBI_ID',
+                'ChEMBL_ID','Drug_Type','PharmGKB_ID','HET_ID','Target_UniProt_ID','Transporter_UniProt_ID',
+                'Enzym_UniProt_ID','Carrier_UniProt_ID')
+
+    credentials = {'user': user, 'passwd': passwd}
+
+    auth_str = base64.b64encode(
+        ('%s:%s' % (credentials['user'], credentials['passwd'])).encode()
+    ).decode()
+
+    decoded = 'Basic %s' % auth_str
+
+    req_hdrs = ['Authorization: %s' % decoded]
+    req_hdrs.extend([settings.get('user_agent')])
+
+    url = urls.urls['drugbank']['all_structures']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".zip")
+    structure_links = list(csv.DictReader(zipfile.result["structure links.csv"], delimiter = ','))
+
+    url = urls.urls['drugbank']['all_drug']
+    c = curl.Curl(
+        url,
+        large = True,
+        silent = False,
+        req_headers = req_hdrs,
+        cache = False
+    ) 
+
+    os.rename(c.fileobj.name, c.fileobj.name + ".zip")
+    zipfile = curl.FileOpener(c.fileobj.name + ".zip")
+    drug_links = list(csv.DictReader(zipfile.result["drug links.csv"], delimiter = ','))
+
+    if addprotid:
+
+        Combine = collections.namedtuple('Combine', fields,defaults = ("",) * len(fields))
+
+    else:
+        Combine = collections.namedtuple('Combine', fields[:17],defaults = ("",) * len(fields[:17]))
+
+    result = []
+    
+    for struct_attr in structure_links:
+
+        for drug_attr in drug_links:
+
+            if struct_attr['DrugBank ID'] == drug_attr['DrugBank ID']:
+
+                result.append(
+                    Combine(
+                        DrugBank_ID = struct_attr['DrugBank ID'],
+                        Name = struct_attr['Name'],
+                        CAS_Number = struct_attr['CAS Number'],
+                        Drug_Groups = struct_attr['Drug Groups'],
+                        InChIKey = struct_attr['InChIKey'],
+                        InChI = struct_attr['InChI'],
+                        SMILES = struct_attr['SMILES'],
+                        Formula = struct_attr['Formula'],
+                        KEGG_Compound_ID = struct_attr['KEGG Compound ID'],
+                        KEGG_Drug_ID = struct_attr['KEGG Drug ID'],
+                        PubChem_Compound_ID = struct_attr['PubChem Compound ID'],
+                        PubChem_Substance_ID = struct_attr['PubChem Substance ID'],
+                        ChEBI_ID = struct_attr['ChEBI ID'],
+                        ChEMBL_ID = struct_attr['ChEMBL ID'],
+                        Drug_Type = drug_attr['Drug Type'],
+                        PharmGKB_ID = drug_attr['PharmGKB ID'],
+                        HET_ID = drug_attr['HET ID'],
+                    )
+                )
+    
+    if addprotid:
+        
+        identifiers_list = add_prot_id(user, passwd, pharma_active)
+        index = 0
+        
+        for res_attr in result:
+
+            for iden_attr in identifiers_list:
+
+                if res_attr.DrugBank_ID == iden_attr.DrugBank_ID:
+                
+                    result[index] = result[index]._replace(
+                        Target_UniProt_ID = iden_attr.Target_UniProt_ID,
+                        Transporter_UniProt_ID = iden_attr.Transporter_UniProt_ID,
+                        Enzym_UniProt_ID = iden_attr.Enzym_UniProt_ID,
+                        Carrier_UniProt_ID = iden_attr.Carrier_UniProt_ID,  
+                    )
+                
+                    break
+
+            index += 1
+
+
+    return result
