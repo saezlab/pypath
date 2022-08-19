56f2ee1731576bc14a585a7a810f5ded3d0b2183
deeenes <turei.denes@gmail.com>
2022-08-19 21:18:44 +0200

    urls: long lines

--- pypath/resources/urls.py
+++ pypath/resources/urls.py
@@ -1528,23 +1528,32 @@ urls = {
     },
     'interpro': {
         'label': 'Protein families, domains and functional sites',
-        'entries': 'https://ftp.ebi.ac.uk/pub/databases/interpro/interpro.xml.gz',
-        'annotations': 'https://www.ebi.ac.uk/interpro/api/entry/InterPro/protein/'
-            '%s/taxonomy/uniprot/%u?page_size=%u',
+        'entries': 'https://ftp.ebi.ac.uk/pub/databases/'
+            'interpro/interpro.xml.gz',
+        'annotations': 'https://www.ebi.ac.uk/interpro/api/entry/InterPro/'
+            'protein/%s/taxonomy/uniprot/%u?page_size=%u',
     },
     'drugcentral': {
         'label': 'Drug-target interactions',
-        'interactions': 'https://unmtid-shinyapps.net/download/DrugCentral/2021_09_01/drug.target.interaction.tsv.gz',
-        'SMILES_InChI' : 'https://unmtid-shinyapps.net/download/DrugCentral/2021_09_01/structures.smiles.tsv',
+        'interactions': 'https://unmtid-shinyapps.net/download/DrugCentral'
+            '/2021_09_01/drug.target.interaction.tsv.gz',
+        'SMILES_InChI' : 'https://unmtid-shinyapps.net/download/DrugCentral'
+            '/2021_09_01/structures.smiles.tsv',
     },
     'drugbank': {
         'label': 'DrugBank database',
-        'all_structures': 'https://go.drugbank.com/releases/5-1-9/downloads/all-structure-links',
-        'all_drug': 'https://go.drugbank.com/releases/5-1-9/downloads/all-drug-links',
-        'drug_target_identifiers' : 'https://go.drugbank.com/releases/5-1-9/downloads/target-all-polypeptide-ids',
-        'drug_enzym_identifiers' : 'https://go.drugbank.com/releases/5-1-9/downloads/enzyme-all-polypeptide-ids',
-        'drug_carrier_identifiers' : 'https://go.drugbank.com/releases/5-1-9/downloads/carrier-all-polypeptide-ids',
-        'drug_transporter_identifiers' : 'https://go.drugbank.com/releases/5-1-9/downloads/transporter-all-polypeptide-ids',
+        'all_structures': 'https://go.drugbank.com/releases/5-1-9/'
+            'downloads/all-structure-links',
+        'all_drug': 'https://go.drugbank.com/releases/5-1-9/downloads/'
+            'all-drug-links',
+        'drug_target_identifiers' : 'https://go.drugbank.com/releases/'
+            '5-1-9/downloads/target-all-polypeptide-ids',
+        'drug_enzym_identifiers' : 'https://go.drugbank.com/releases/5-1-9/'
+            'downloads/enzyme-all-polypeptide-ids',
+        'drug_carrier_identifiers' : 'https://go.drugbank.com/releases/5-1-9/'
+            'downloads/carrier-all-polypeptide-ids',
+        'drug_transporter_identifiers' : 'https://go.drugbank.com/releases/'
+            '5-1-9/downloads/transporter-all-polypeptide-ids',
     }, 
     'chembl': {
         'label': 'ChEMBL database',
@@ -1556,9 +1565,11 @@ urls = {
     },
     'hpo': {
         'label': 'HPO database',
-        'ontology': 'https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo',
+        'ontology': 'https://raw.githubusercontent.com/obophenotype/'
+            'human-phenotype-ontology/master/hp.obo',
         'disease' : 'http://purl.obolibrary.org/obo/hp/hpoa/phenotype.hpoa',
-        'gene' : 'http://purl.obolibrary.org/obo/hp/hpoa/genes_to_phenotype.txt',
+        'gene' : 'http://purl.obolibrary.org/obo/hp/hpoa/'
+            'genes_to_phenotype.txt',
     },
 }
 
