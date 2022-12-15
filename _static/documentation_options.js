278037af6fe38202e407668522a49a18a8587ef7
deeenes <turei.denes@gmail.com>
2022-12-15 15:58:03 +0000

    Update gh-pages to output generated at bcd78c0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.14.33',
+    VERSION: '0.14.34',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
