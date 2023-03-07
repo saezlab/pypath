ba8c0c2fe4ea8a68f84931c0801626edfe788566
deeenes <turei.denes@gmail.com>
2023-03-07 05:15:36 +0100

    `ramp`: more efficient mysqldump processing

--- pypath/inputs/ramp.py
+++ pypath/inputs/ramp.py
@@ -35,6 +35,7 @@ import collections
 import re
 
 import sqlparse
+# import pymysql
 import pandas as pd
 
 import pypath.resources.urls as urls
@@ -67,10 +68,10 @@ def ramp_raw(tables: list[str] = None) -> dict[str, pd.DataFrame]:
         A dictionary with the table names as keys and pandas dataframes as values.
     """
 
-    return _sqldump_table(_ramp_sqldump(), tables)
+    return _sqldump_tables(_ramp_sqldump(), tables, return_df = True)
 
 
-def ramp_tables() -> dict[str, list[str]]:
+def ramp_list_tables() -> dict[str, list[str]]:
     """
     List the tables of the RaMP database from SQL dump.
     """
@@ -78,7 +79,108 @@ def ramp_tables() -> dict[str, list[str]]:
     return _sqldump_list_tables(_ramp_sqldump())
 
 
-def _sqldump_table(
+def _sqldump_tables(
+        sqldump: str | IO,
+        tables: str | Iterable[str] | None,
+        return_df: bool = True,
+        **kwargs
+    ) -> tuple[dict, dict] | dict[str, pd.DataFrame]:
+    """
+    From a SQL dump, retrieve tables as data frames or as lists of tuples.
+
+    Args:
+        sqldump: SQL dump file. Path or file-like object.
+        table: Name of the table.
+        return_df: If True, return dict of pandas dataframes.
+        **kwargs: Passed to `pypath.share.curl.FileOpener`.
+
+    Returns:
+        Contents of the table.
+    """
+
+    sqldump = _sqldump_open(sqldump, **kwargs)
+    tables = common.to_set(tables) or _sqldump_list_tables(sqldump)
+
+    contents = {}
+    headers = {}
+
+    for table in tables:
+
+        _sqldump_seek(sqldump, table)
+        the_table = _sqldump_one_table(sqldump, return_df)
+        contents[table] = the_table if return_df else the_table[1]
+        headers[table] = None if return_df else the_table[0]
+
+        if not tables - set(contents.keys()):
+
+            break
+
+    return contents if return_df else headers, contents
+
+
+def _sqldump_one_table(
+        sqldump: IO,
+        return_df: bool = True,
+    ) -> tuple[list[str], list[tuple]] | pd.DataFrame:
+
+    parser = sqlparse.parsestream(sqldump)
+    header = []
+    content = []
+    inserts = 0
+
+    for statement in parser:
+
+        if statement.get_type() == 'INSERT':
+
+            inserts += 1
+            sublists = statement.get_sublists()
+            table_info = next(sublists)
+            table_name = table_info.get_name()
+
+            if inserts == 1:
+                
+                the_table_name = table_name
+                _log(f'Processing table: {table_name}')
+
+            if table_name != the_table_name:
+
+                break
+
+            header = [
+                col.get_name()
+                for col in table_info.get_parameters()
+            ]
+
+            content.extend(
+                tuple(
+                    s.value.strip('"\'')
+                    for s in next(rec.get_sublists()).get_identifiers()
+                )
+                for rec in next(sublists).get_sublists()
+            )
+
+    _log(
+        f'Processed {len(content)} records in {inserts} INSERT statements '
+        f'from table {table_name}.'
+    )
+
+    if return_df:
+
+        df = pd.DataFrame.from_records(content, columns = header)
+
+        _log(
+            f'Data frame of table {table_name}: '
+            f'{df.shape[0]} records, {common.df_memory_usage(df)}.'
+        )
+
+        return df
+
+    else:
+
+        return header, content
+
+
+def _sqldump_table_2(
         sqldump: str | IO,
         tables: str | Iterable[str],
         return_df: bool = True,
@@ -100,63 +202,15 @@ def _sqldump_table(
     tables = common.to_set(tables)
     sqldump = _sqldump_open(sqldump, **kwargs)
 
+    con = pymysql.connect(
+        
+    )
+
     contents = collections.defaultdict(list)
     headers = {}
 
-    parser = sqlparse.parsestream(sqldump)
 
-    for statement in parser:
 
-        if not tables - set(contents.keys()):
-
-            break
-
-        if statement.get_type() == 'INSERT':
-
-            sublists = statement.get_sublists()
-            table_info = next(sublists)
-            table_name = table_info.get_name()
-            _log(f'Found table: {table_name}')
-
-            if table_name not in tables:
-
-                _log(f'Skipping table {table_name}.')
-                continue
-            
-            _log(f'Processing table: {table_name}')
-
-            headers[table_name] = [
-                col.get_name()
-                for col in table_info.get_parameters()
-            ]
-
-            contents[table_name].extend(
-                tuple(
-                    s.value.strip('"\'')
-                    for s in next(rec.get_sublists()).get_identifiers()
-                )
-                for rec in next(sublists).get_sublists()
-            )
-    
-    if return_df:
-
-        contents = {
-            name: pd.DataFrame.from_records(records, columns = headers[name])
-            for name, records in contents.items()
-        }
-
-        for name, df in contents.items():
-
-            _log(
-                f'Data frame of table {name}: '
-                f'{df.shape[0]} records, {common.df_memory_usage(df)}.'
-            )
-
-        return contents
-
-    else:
-
-        return headers, dict(contents)
 
 
 def _sqldump_list_tables(sqldump: str | IO) -> dict[str, list[str]]:
@@ -212,3 +266,34 @@ def _sqldump_open(sqldump: str | IO, **kwargs) -> IO:
 
     return sqldump
 
+
+def _sqldump_seek(sqldump: IO, table: str, insert: bool = False) -> None:
+    """
+    Seek to a table in a SQL dump file.
+
+    Args:
+        sqldump:
+            SQL dump file.
+        table:
+            Name of the table.
+        insert:
+            If True, seek to the beginning of the INSERT statement.
+    """
+
+    seek_for = f'INSERT INTO `{table}`' if insert else f'DROP TABLE IF EXISTS `{table}`'
+    beginning_of_line = 0
+
+    line = sqldump.readline()
+
+    while line:
+
+        if line.startswith(seek_for):
+
+            sqldump.seek(beginning_of_line)
+            return
+
+        beginning_of_line = sqldump.tell()
+        line = sqldump.readline()
+
+    sqldump.seek(0)
+    _sqldump_seek(sqldump, table, insert)
