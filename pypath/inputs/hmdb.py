065cd1d2352239aa820b9a18d5b4709f9de7c7fa
deeenes <turei.denes@gmail.com>
2023-03-19 12:47:43 +0100

    `hmdb_table`: custom data frames from HMDB

--- pypath/inputs/hmdb.py
+++ pypath/inputs/hmdb.py
@@ -29,12 +29,16 @@ from __future__ import annotations
 Access the Human Metabolome Database.
 """
 
-from typing import Literal
+from typing import Any, Literal
+
+import itertools
 
 from lxml import etree
+import pandas as pd
 
 import pypath.resources.urls as urls
 import pypath.share.curl as curl
+import pypath.share.common as common
 import pypath.share.session as session
 import pypath.formats.xml as xml
 
@@ -298,7 +302,7 @@ SCHEMA.update(xml._simple_fields(SIMPLE_FIELDS))
 SCHEMA.update(xml._array_fields(ARRAY_FIELDS))
 
 
-def hmdb_metabolites(schema: dict = None, head: int | None = None) -> list:
+def hmdb_metabolites_raw(schema: dict = None, head: int | None = None) -> list:
     """
     Parse metabolite data from HMDB.
 
@@ -324,10 +328,171 @@ def hmdb_metabolites(schema: dict = None, head: int | None = None) -> list:
 
     for i, met in enumerate(hmdb_iter_metabolites()):
 
-        result.append(xml.fetch(met, schema, XMLNS))
+        yield xml.fetch(met, schema, XMLNS)
 
         if i == head:
 
             break
 
     return result
+
+
+class Field:
+
+    def __init__(self, name, *definition):
+
+        if isinstance(name, Field):
+
+            name, definition = name.name, name.definition
+
+        self.name = name
+        self.d = definition or (name,)
+
+
+    def process(self, record) -> tuple[tuple[Any]]:
+
+        value = ((record,),)
+        name = common.to_tuple(self.name)
+
+        for d in self.d:
+
+            if d == '@':
+
+                value = tuple(
+                    v[0]
+                        if isinstance(v[0], (list, tuple)) else v
+                    for v in value
+                )
+
+            elif d == '*' or isinstance(d, tuple):
+
+                if d == '*':
+
+                    d = tuple(sorted(value[0].keys()))
+
+                if len(value) > 1:
+
+                    _log('List of dicts content encountered.')
+
+                value = value[0]
+                value = tuple(tuple(v.get(k) for v in value) for k in d)
+                name = tuple(f'{n}__{k}' for n in name for k in d)
+
+            elif isinstance(d, str):
+
+                value = tuple(tuple(v.get(d) for v in vv) for vv in value)
+
+        return name, value
+
+
+    def __str__(self):
+
+        return self.name
+
+
+def hmdb_table(
+        *fields: str | tuple,
+        head: int | None = None,
+        **named_fields: str | tuple,
+    ) -> pd.DataFrame:
+    """
+    Parse various simple and nested array fields from HMDB into data frame.
+
+    Args:
+        fields:
+            Fields to include in the data frame. These must be keys in the
+            schema, and will be also used as column names. Alternatively,
+            tuples of sequetial processing steps can be provided: strings
+            will be used as keys in nested dicts, tuples will be used as
+            multiple keys in dicts, each yielding a separate column, the
+            special symbol "*" means all keys in the sub-dict, while "@"
+            means expand arrays into multiple rows. Be careful with this
+            latter option because it is applied in a combinatorial way, i.e.
+            in case of expanding an array to 5 rowns, and another one to 7
+            rows results already 35 rows from a single record. This might
+            result excessive memory use and processing time.
+        named_fields:
+            Same as `fields`, but the column name can be different from the
+            top level key: argument names will be used as column names,
+            values will be used as processing steps.
+        head:
+            Process the first N records only. Useful for peeking into
+            the data.
+
+    Examples:
+
+        ..code-block:: python
+
+           from pypath.inputs import hmdb
+           df = hmdb.hmdb_table('accession', 'smiles', 'state', head = 10)
+           df = hmdb.hmdb_table('accession', ('synonyms', '@'), head = 10)
+           df = hmdb.hmdb_table(
+               'accession',
+               ('taxonomy', ('class', 'substituents')),
+               head = 10,
+           )
+
+    """
+
+    fields = [Field(d[0], *d) for d in (common.to_tuple(f) for f in fields)]
+    fields.extend(Field(n, *f) for n, f in named_fields.items())
+    keys = [f.name for f in fields]
+    schema = {k: SCHEMA[k] for k in keys}
+
+    columns = []
+    data = []
+    result = pd.DataFrame()
+
+    for i, met in enumerate(hmdb_metabolites_raw(schema, head = head)):
+
+        cols, rows = zip(*(f.process(met) for f in fields))
+        columns = columns or list(itertools.chain(*cols))
+        data.extend(itertools.product(*itertools.chain(*rows)))
+
+        if (i + 1) % 100 == 0:
+
+            df = pd.DataFrame.from_records(data, columns = columns)
+            result = pd.concat((result, df))
+            data = []
+
+    df = pd.DataFrame.from_records(data, columns = columns)
+    result = pd.concat((result, df))
+
+    return result
+
+
+def hmdb_mapping(
+        id_type_a: str,
+        id_type_b: str,
+        return_df: bool = False,
+    ) -> dict[str, set[str]] | pd.DataFrame:
+    """
+    ID translation input from HMDB.
+
+    Note: you can use this function for purposes other than ID translation
+    tables, e.g. you can collect the molecular weights, pathways, etc. Though
+    not all these options are guaranteed to work or result a meaningful
+    output.
+
+    Args:
+        id_type_a:
+            An identifier type, see the `ID_FIELDS` set in this module,
+            and keys in the `SIMPLE_FIELDS` dict.
+        id_type_b:
+            Another identifier type, same options as for `id_type_a`.
+        return_df:
+            Return a data frame instead of dict of sets.
+
+    Return:
+        Translation data between two types of identifiers.
+    """
+
+
+
+
+def _id_type(id_type: str) -> str:
+    """
+    Field name from ID type.
+    """
+
+    return SCHEMA.get('id_type', SCHEMA.get(f'{id_type}_id'))
