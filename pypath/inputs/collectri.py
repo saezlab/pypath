2e7cabf449e356bbf050853473d7d492dd400f43
deeenes <turei.denes@gmail.com>
2023-03-22 22:32:48 +0100

    refactored `inputs.collectri`

--- pypath/inputs/collectri.py
+++ pypath/inputs/collectri.py
@@ -5,7 +5,7 @@
 #  This file is part of the `pypath` python module
 #
 #  Copyright
-#  2014-2022
+#  2014-2023
 #  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
 #
 #  Authors: Dénes Türei (turei.denes@gmail.com)
@@ -23,6 +23,14 @@
 #  Website: http://pypath.omnipathdb.org/
 #
 
+from __future__ import annotations
+
+"""
+CollecTRI is a comprehensive resource of TF-target interactions.
+"""
+
+from typing import Literal
+
 import re
 import collections
 
@@ -30,7 +38,22 @@ import pypath.resources.urls as urls
 import pypath.share.curl as curl
 
 
-def collectri_interactions(int_type = 'gene'):
+def collectri_interactions(
+        protein_coding: bool = True,
+        mirna: bool = False,
+    ) -> list[tuple]:
+    """
+    TF-target interactions from the CollecTRI database.
+
+    Args:
+        protein_coding:
+            Include regulation of protein coding genes.
+        mirna:
+            Include regulation of miRNA coding genes.
+    """
+
+
+    remirna = re.compile('^MIR(\d+)$')
 
     CollectriInteraction = collections.namedtuple(
         'CollectriInteraction',
@@ -41,6 +64,7 @@ def collectri_interactions(int_type = 'gene'):
             'TF_category',
             'resources',
             'pubmed',
+            'target_type',
         ),
     )
 
@@ -52,29 +76,31 @@ def collectri_interactions(int_type = 'gene'):
     )
 
     result = []
-    
-    if int_type == 'miRNA':
-        for l in c.result:
-            l = l.strip().split(',')
-            match = re.match('^MIR(\d+)$', l[1])
-            
-            if match:
-                number = match.group(1)
-                l[1] = 'hsa-miR-' + number
-                result.append(
-                    CollectriInteraction(l[0], l[1], int(l[2]), l[3], l[4], l[5])
-                    )
-                
-        return result
 
-    if int_type == 'gene':
-        for l in c.result:
-            l = l.strip().split(',')
-            
-            
-            if l[0] != 'source':
-                result.append(
-                    CollectriInteraction(l[0], l[1], int(l[2]), l[3], l[4], l[5])
-                    )
-                
-        return result
+    for l in c.result:
+
+        l = l.strip().split(',')
+        mmirna = remirna.match(l[1])
+
+        if (
+            (mmirna and not mirna) or
+            (not mmirna and not protein)
+        ):
+
+            continue
+
+        target_id = f'hsa-miR-{mmirna.group(1)}' if mmirna else l[1]
+
+        result.append(
+            CollectriInteraction(
+                tf = l[0],
+                target = target_id,
+                effect = int(l[2]),
+                tf_category = l[3],
+                resources = l[4],
+                pubmed = l[5],
+                target_type = 'mirna' if mmirna else 'protein',
+            )
+        )
+
+    return result
