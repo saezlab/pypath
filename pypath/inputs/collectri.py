1321559824befbf09179400082adeb373b9d3414
deeenes <turei.denes@gmail.com>
2023-03-27 14:24:09 +0200

    `collectri`: process complexes into `Complex` objects

--- pypath/inputs/collectri.py
+++ pypath/inputs/collectri.py
@@ -33,12 +33,59 @@ from typing import Literal
 
 import re
 import collections
+import itertools
 
 import pypath.resources.urls as urls
 import pypath.share.curl as curl
+import pypath.internals.intera as intera
+import pypath.utils.mapping as mapping
 
 
-def collectri_interactions(
+# Based on literature by Sophia
+# https://www.sciencedirect.com/science/article/abs/pii/S0304419X19300526
+# https://www.nature.com/articles/1209933
+COMPLEXES = {
+    'AP1': {
+        'JUN-FOS',
+        'JUNB-FOS',
+        'JUND-FOS',
+        'JUN-FOSB',
+        'JUNB-FOSB',
+        'JUND-FOSB',
+        'JUN-FOSL1',
+        'JUNB-FOSL1',
+        'JUND-FOSL1',
+        'JUN-FOSL2',
+        'JUNB-FOSL2',
+        'JUND-FOSL2',
+        'JUN-JUN',
+        'JUN-JUNB',
+        'JUN-JUND',
+        'JUNB-JUNB',
+        'JUNB-JUND',
+        'JUND-JUND',
+    },
+    'NFKB': {
+        'RELA-RELA',
+        'RELA-REL',
+        'RELA-RELB',
+        'RELA-NFKB1',
+        'RELA-NFKB2',
+        'REL-REL',
+        'REL-RELB',
+        'REL-NFKB1',
+        'REL-NFKB2',
+        'RELB-RELB',
+        'RELB-NFKB1',
+        'RELB-NFKB2',
+        'NFKB1-NFKB1',
+        'NFKB1-NFKB2',
+        'NFKB2-NFKB2',
+    },
+}
+
+
+def collectri_raw(
         protein_coding: bool = True,
         mirna: bool = False,
     ) -> list[tuple]:
@@ -55,8 +102,8 @@ def collectri_interactions(
 
     remirna = re.compile('^MIR(\d+)$')
 
-    CollectriInteraction = collections.namedtuple(
-        'CollectriInteraction',
+    CollectriRecord = collections.namedtuple(
+        'CollectriRecord',
         (
             'tf',
             'target',
@@ -93,7 +140,7 @@ def collectri_interactions(
         target_id = f'hsa-miR-{mmirna.group(1)}' if mmirna else l[1]
 
         result.append(
-            CollectriInteraction(
+            CollectriRecord(
                 tf = l[0],
                 target = target_id,
                 effect = int(l[2]),
@@ -105,3 +152,67 @@ def collectri_interactions(
         )
 
     return result
+
+
+def collectri_interactions(
+        protein_coding: bool = True,
+        mirna: bool = False,
+    ) -> list[tuple]:
+    """
+    TF-target interactions from the CollecTRI database.
+
+    While `collectri_raw` returns the records in the same format as in the
+    original data, here we translate identifiers to UniProt IDs, and use
+    `Complex` objects to represent protein complexes.
+
+    Args:
+        protein_coding:
+            Include regulation of protein coding genes.
+        mirna:
+            Include regulation of miRNA coding genes.
+    """
+
+    CollectriInteraction = collections.namedtuple(
+        'CollectriInteraction',
+        (
+            'tf',
+            'target',
+            'effect',
+            'tf_category',
+            'resources',
+            'pubmed',
+            'target_type',
+        ),
+    )
+
+    for rec in collectri_raw(protein_coding = protein_coding, mirna = mirna):
+
+        tf_uniprots = (
+            set(itertools.chain(*(
+                itertools.product(*(
+                    mapping.map_name(comp, 'genesymbol', 'uniprot')
+                    for comp in components.split('-')
+                ))
+                for components in COMPLEXES[rec.tf]
+            )))
+            if rec.tf in COMPLEXES else
+                mapping.map_name(rec.tf, 'genesymbol', 'uniprot')
+        )
+
+        target_uniprots = (
+            (rec.target,)
+                if rec.target_type == 'mirna' else
+            mapping.map_name(rec.target, 'genesymbol', 'uniprot')
+        )
+
+        for tf_u, t_u in itertools.product(tf_uniprots, target_uniprots):
+
+            if isinstance(tf_u, tuple):
+
+                tf_u = intera.Complex(
+                    components = tf_u,
+                    sources = 'CollecTRI',
+                )
+
+                yield CollectriInteraction(tf_u, t_u, *rec[2:])
+
