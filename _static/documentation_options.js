eb34c5d72b4e9bad3f18769f06eb1331448330b9
deeenes <turei.denes@gmail.com>
2023-04-03 14:41:59 +0000

    Update gh-pages to output generated at f7029af

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.14.45',
+    VERSION: '0.14.46',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
