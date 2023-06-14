b4af4d6a79f2a02209ad5c6d98e996c1c991902d
deeenes <turei.denes@gmail.com>
2023-06-14 10:25:28 +0200

    preliminary refactoring of `inputs.reactome`

--- pypath/inputs/reactome.py
+++ pypath/inputs/reactome.py
@@ -1,87 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2023
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: Dénes Türei (turei.denes@gmail.com)
+#           Nicolàs Palacio
+#           Sebastian Lobentanzer
+#           Erva Ulusoy
+#           Olga Ivanova
+#           Ahmet Rifaioglu
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
+from __future__ import annotations
+
+from typing import Generator, Literal
+
 import collections
+
+import pandas as pd
 
 import pypath.share.curl as curl
 import pypath.resources.urls as urls
 
 
-class Reactome:
+_molecule_fields =(
+    'pathway_id',
+    'url',
+    'pathway_name',
+    'evidence_code',
+    'organism'
+)
 
-    def __init__(self, source):
+_FIELDS = {
+    'uniprot2all': ('uniprot_id',) + _molecule_fields,
+    'chebi2all': ('chebi_id',) + _molecule_fields,
+    'pathways': (
+        'pathway_id',
+        'pathway_name',
+        'organism',
+    ),
+    'pathway_relations': (
+        'parent',
+        'child',
+    ),
+}
 
-        if source == 'uniprot2all':
-
-            self.url = urls.urls['reactome']['url_uniprot2all']
-            self.fields = [
-                'uniprot_id',
-                'pathway_id',
-                'url',
-                'pathway_name',
-                'evidence_code',
-                'organism'
-            ]
-        
-        elif source == 'chebi2all':
-
-            self.url = urls.urls['reactome']['url_chebi2all']
-            self.fields = [
-                'chebi_id',
-                'pathway_id',
-                'url',
-                'pathway_name',
-                'evidence_code',
-                'organism'
-            ] 
-        
-        elif source == 'pathways':
-
-            self.url = urls.urls['reactome']['url_pathways']
-            self.fields = [
-                'pathway_id',
-                'pathway_name',
-                'organism'
-            ]
-
-        elif source == 'pathway_relations':
-            
-            self.url = urls.urls['reactome']['url_pathway_relations']
-            self.fields = [
-                'parent',
-                'child'
-            ]
-
-        else:
-            raise ValueError('Invalid Input')
+_RECORD_NAMES = {
+    'uniprot2all': 'Uniprot',
+    'chebi2all': 'Chebi',
+    'pathways': 'Pathway',
+    'pathway_relations': 'PathwayRelation',
+}
 
 
-def reactome_data(source) -> list[tuple]:
-    '''
+def _reactome_data(
+        dataset: Literal[
+            'uniprot2all',
+            'chebi2all',
+            'pathways',
+            'pathway_relations',
+        ],
+        return_df: bool = False,
+    ) -> Generator[tuple] | pd.DataFrame:
+    """
     Retrieves pathway information by given input
     Args:
         source:
-        - uniprot2all (fields: uniprot_id, pathway_id, url, pathway_name, evidence_code, organism)
-        - chebi2all (fields: chebi_id, pathway_id, url, pathway_name, evidence_code, organism)
-        - pathways (fields: pathway_id, pathway_name, organism)
-        - pathway_relations (fields: parent, child)
+            - uniprot2all (fields: uniprot_id, pathway_id, url,
+              pathway_name, evidence_code, organism)
+            - chebi2all (fields: chebi_id, pathway_id, url,
+              pathway_name, evidence_code, organism)
+            - pathways (fields: pathway_id, pathway_name, organism)
+            - pathway_relations (fields: parent, child)
+        return_df:
+            Return a pandas data frame.
 
     Returns:
-        list of namedtuples according to source and its fields.
-    '''
+        Records of the requested dataset.
+    """
 
-    source = Reactome(source = source)
-    data = curl.Curl(source.url, large = True).result
+    result = _reactome_data_gen(dataset)
 
-    fields = source.fields
+    return pd.DataFrame(result) if return_df else result
+
+
+def _reactome_data_gen(
+        dataset: Literal[
+            'uniprot2all',
+            'chebi2all',
+            'pathways',
+            'pathway_relations',
+        ]
+    ) -> Generator[tuple]:
+
+    url = urls.urls['reactome'][dataset]
+    c = curl.Curl(url, large = True)
+
 
     result = set()
-    record = collections.namedtuple('Pathway', fields)
+    record = collections.namedtuple('Reactome', fields)
 
-    for line in data:
+    for line in c.result:
 
-        elements = line.strip('\n').split('\t')
-        zipped = dict(zip(fields, elements))
-
-        result.add(
-            record(**dict(zip(fields, (zipped.get(f, None) for f in fields))))
-        )
-    
-    return list(result)
+        line = line.strip('\n').split('\t')
+        yield record(*line)
