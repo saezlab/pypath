d91cce3bc8a03f376abf20f34bb8677ed970fc08
deeenes <turei.denes@gmail.com>
2023-06-21 14:28:27 +0200

    refactoring `inputs.gutmgene`

--- pypath/inputs/gutmgene.py
+++ pypath/inputs/gutmgene.py
@@ -1,84 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright
+#  2014-2023
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: Dénes Türei (turei.denes@gmail.com)
+#           Nicolàs Palacio
+#           Sebastian Lobentanzer
+#           Erva Ulusoy
+#           Olga Ivanova
+#           Ahmet Rifaioglu
+#           Melih Darcan
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: http://pypath.omnipathdb.org/
+#
+
 from __future__ import annotations
+
+from typing import Literal, NamedTuple
 
 import collections
 
 import pypath.share.curl as curl
+import pypath.utils.taxonomy as taxonomy
+import pypath.utils.mapping as mapping
 import pypath.resources.urls as urls
+import pypath.share.session as session
 
-def gutmgene_human_raw() -> list[tuple]:
+_log = session.Logger(name = 'gutmgene_input')._log
+
+
+class GutmgeneRaw(NamedTuple):
+    microbe_taxon: str
+    microbe_ncbi_tax_id: str
+    gut_microbiota_id: str
+    classification: str
+    genesymbol: str
+    entrez: str
+    effect: str
+    throughput: str
+
+
+class GutmgeneAnnotation(NamedTuple):
+    microbe_taxon: str
+    microbe_ncbi_tax_id: str
+    gut_microbiota_id: str
+    classification: str
+    effect: str
+    throughput: str
+
+
+def gutmgene_raw(organism: Literal['human', 'mouse'] = 'human') -> list[tuple]:
     """
-    Retrieves raw data about gut microbiota genes in humans from the gut microbiota gene database.
+    Gut microbiota genes from the gut microbiota gene database (gutMGene).
+
+    Args:
+        organism: Organism ID or name; human and mouse are available.
 
     Returns:
-    A list of named tuples containing information about gut microbiota genes in human.
+        A list of named tuples containing information about gut microbiota
+        genes in human.
     """
-    HumanFields = collections.namedtuple(
-        'gutMGeneHuman',
-        [
-            'gut_microbiota',
-            'gut_microbiata_tax_id',
-            'gut_microbiota_id',
-            'classification',
-            'gene_symbol',
-            'gene_entrez_id',
-            'alteration',
-            'high_low_throughput',
-        ],
-        defaults = None
-    )
-    
-    url = urls.urls['gutmgene']['url_human']
+
+    organism_ = taxonomy.ensure_common_name(organism, lower = True)
+
+    if organism not in ('human', 'mouse'):
+
+        err = '`organism` must be either `human` or `mouse`, not `{organism}`.'
+        _log(err)
+        raise ValueError(err)
+
+    url = urls.urls['gutmgene'][f'url_{organism_}']
     c = curl.Curl(url, silent = False, large = True)
-    
+
     result = set()
-        
+
     for l in c.result:
-        l = l.strip().split('\t')
-        l = [None if not i else i for i in l]
-        if l:
-            result.add(HumanFields(*l))
-            
-    return list(result)
 
-
-def gutmgene_mouse_raw() -> list[tuple]:
-    """
-    Retrieves raw data about gut microbiota genes in mice from the gut microbiota gene database.
-
-    Returns:
-    A list of named tuples containing information about gut microbiota genes in mouse.
-    """
-
-    MouseFields = collections.namedtuple(
-        'gutMGeneMouse',
-        [
-            'gut_microbiota',
-            'gut_microbiata_tax_id',
-            'gut_microbiota_id',
-            'classification',
-            'gene_symbol',
-            'gene_entrez_id',
-            'alteration',
-            'high_low_throughput',
-        ],
-        defaults = None
-    )
-    
-    url = urls.urls['gutmgene']['url_mouse']
-    c = curl.Curl(url, silent = False, large = True)
-    
-    result = set()
-        
-    for l in c.result:
         if l.startswith('"'):
             continue
-        
+
         l = l.replace('"', '')
         l = l.strip().split('\t')
-        l = [None if not i else i for i in l]
+        l = (None if not i else i for i in l)
+
         if l:
-            result.add(MouseFields(*l))
-            
+
+            result.add(GutmgeneRaw(*l))
+
     return list(result)
-            
-        
+
+
+def gutmgene_annotations(
+        organism: Literal['human', 'mouse'] = 'human',
+    ) -> dict[str, set[GutmgeneAnnotation]]:
+    """
+    Microbial effectors of human or mouse genes from the gutMGene database.
+
+    Args:
+        organism:
+            Organism ID or name; human and mouse are available.
+
+    Return:
+        A dict of sets of named tuples representing microbial relationships;
+        top level keys are UniProt IDs.
+    """
+
+    raw = gutmgene_raw(organism)
+    ncbi_tax_id = taxonomy.ensure_ncbi_tax_id(organism)
+    result = collections.defaultdict(set)
+
+    for rec in raw:
+
+        uniprots = mapping.map_name(
+            rec.genesymbol,
+            'genesymbol',
+            'uniprot',
+            ncbi_tax_id = ncbi_tax_id,
+        )
+
+        for uniprot in uniprots:
+
+            result[uniprot].add(
+                GutmgeneAnnotation(
+                    microbe_taxon = rec.microbe_taxon,
+                    microbe_ncbi_tax_id = rec.microbe_ncbi_tax_id,
+                    gut_microbiota_id = rec.gut_microbiota_id,
+                    classification = rec.classification,
+                    effect = rec.effect,
+                    throughput = rec.throughput,
+                )
+            )
+
+    return dict(result)
