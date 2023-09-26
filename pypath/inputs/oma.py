b45bef0a7dc222381ce942d59531b2d2890dc3f0
deeenes <turei.denes@gmail.com>
2023-09-26 18:36:38 +0200

    `oma_orthologs`: more features and variables

--- pypath/inputs/oma.py
+++ pypath/inputs/oma.py
@@ -21,44 +21,73 @@ from __future__ import annotations
 
 from typing import Literal
 import collections
+import itertools
+
+import pandas as pd
 
 import pypath.resources.urls as urls
 import pypath.share.curl as curl
 import pypath.share.common as common
 import pypath.inputs.common as inputs_common
 import pypath.utils.taxonomy as taxonomy
+import pypath.utils.mapping as mapping
 
 
 def oma_orthologs(
-        organism_a: str | int,
-        organism_b: str | int,
+        organism_a: str | int = 'human',
+        organism_b: str | int = 'mouse',
+        id_type: str | None = None,
         rel_type: set[Literal['1:1', '1:n', 'm:1', 'm:n']] | None = None,
-        score: float = None
-    ) -> list[tuple]:
+        score: float = None,
+        return_df: bool = False,
+    ) -> list[tuple] | pd.DataFrame:
     """
     Retrieves pairwise relations between two genomes from the OMA
     (Orthologous Matrix) database (https://omabrowser.org/oma/home/).
 
     Args:
-        genome_id_a:
+        organism_a:
             Name or NCBI Taxonomy ID of the first organism.
-        genome_id_b:
+        organism_b:
             Name or NCBI Taxonomy ID of the second organism.
+        id_type:
+            OMA by default uses UniProt entry IDs and sometimes other
+            identifiers for genes. Set this parameter to control which
+            ID type all the identifiers are to be mapped to.
         rel_type:
             Restrict relations to certain types.
         score:
-            Resemblance metric.
+            Lower threshold for similarity metric.
+        return_df:
+            If True, returns a data frame instead of a list of tuples.
 
     Returns:
-        A list with tuples of pairwise orthologous relationships.
+        A list with tuples of pairwise orthologous relationships or a data
+        frame with the same records.
     """
 
-    OmaOrthologs = collections.namedtuple(
-            'OmaOrthologs',
+    OmaGene = collections.namedtuple(
+        'OmaGene',
+        (
+            'id',
+            'oma_group',
+            'hog',
+            'taxon',
+            'chr',
+            'start',
+            'end',
+            'strand',
+            'main_isoform',
+        )
+    )
+
+    OmaOrthology = collections.namedtuple(
+            'OmaOrthology',
             (
-                'id_a',
-                'id_b',
+                'a',
+                'b',
                 'rel_type',
+                'dist',
                 'score',
             ),
         )
@@ -85,20 +114,92 @@ def oma_orthologs(
         page += 1
         data = inputs_common.json_read(c.result)
 
-        result.update({
-            OmaOrthologs(
-                id_a = i['entry_1']['canonicalid'],
-                id_b = i['entry_2']['canonicalid'],
-                rel_type = i['rel_type'],
-                score = float(i['score']),
-            )
-            for i in data
+        for rec in data:
+
             if (
-                (not score or i['score'] >= score) and
-                (not rel_type or i['rel_type'] in rel_type)
+                (score and rec['score'] < score) or
+                (rel_type and rec['rel_type'] not in rel_type)
+            ):
+                continue
+
+            a, b = (
+                [
+                    OmaGene(
+                        id = id_,
+                        oma_group = e['oma_group'],
+                        hog = e['oma_hog_id'],
+                        taxon = e['species']['taxon_id'],
+                        chr = e['chromosome'],
+                        start = int(e['locus']['start']),
+                        end = int(e['locus']['end']),
+                        strand = int(e['locus']['strand']),
+                        main_isoform = e['is_main_isoform'],
+
+                    )
+                    for id_ in _id_translate(
+                        id_ = e['canonicalid'],
+                        taxon = e['species']['taxon_id'],
+                        id_type = id_type,
+                    )
+                ]
+                for e in (rec[f'entry_{ei}'] for ei in (1, 2))
             )
-        })
+
+
+            result.update(
+                {
+                    OmaOrthology(
+                        a = _a,
+                        b = _b,
+                        rel_type = rec['rel_type'],
+                        dist = float(rec['distance']),
+                        score = float(rec['score']),
+                    )
+                    for _a, _b in itertools.product(a, b)
+                }
+            )
 
         if page > n_pages: break
 
-    return list(result)
+    result = list(result)
+
+    if return_df:
+
+        result = pd.DataFrame(result)
+        result = pd.concat(
+            [
+                pd.DataFrame(result.a.tolist()).add_suffix('_a', axis = 1),
+                pd.DataFrame(result.b.tolist()).add_suffix('_b', axis = 1),
+                result.drop(['a', 'b'], axis = 1),
+            ],
+            axis = 1,
+        )
+
+    return result
+
+
+def _id_translate(id_: str, taxon: int, id_type: str | None) -> set[str]:
+
+    if not id_type: return {id_}
+
+    s_id_type = (
+        'ensg'
+            if id_.startswith('ENS') else
+        'uniprot-entry'
+            if '_' in id_ else
+        'uniprot'
+    )
+
+    uniprots = mapping.map_name(
+        id_,
+        s_id_type,
+        'uniprot',
+        ncbi_tax_id = taxon,
+    )
+
+    return mapping.map_names(
+        uniprots,
+        'uniprot',
+        id_type,
+        ncbi_tax_id = taxon,
+    ) if uniprots else set()
