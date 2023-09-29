saezlab/pypath

commit ccae9ed63b6d0d51f764f705ce8db16a8df7b7fb

`taxonomy`: some NCBI taxons have multiple common names; selecting the shortest when translating to common name

pypath/utils/taxonomy.py
@@ -524,7 +524,8 @@ def init_db(key):
 
     if swap:
 
-        this_db = common.swap_dict_simple(this_db)
+        this_db = common.swap_dict(this_db)
+        this_db = {k: min(v, key = len) for k, v in this_db.items()}
 
     else:
 

commit 76a23667603fb54c5bd76dd4e12265012c020233

`taxonomy`: fixed bug introduced in ccae9ed that compromised to ensembl translation

pypath/utils/taxonomy.py
@@ -524,7 +524,7 @@ def init_db(key):
 
     if swap:
 
-        this_db = common.swap_dict(this_db)
+        this_db = common.swap_dict(this_db, force_sets = True)
         this_db = {k: min(v, key = len) for k, v in this_db.items()}
 
     else: