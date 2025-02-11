44c1a9612cdd8c4c6989fb44b454ad47ebf36f19
Macabe Daley <37773554+maca8e@users.noreply.github.com>
2025-02-11 15:55:08 +0100

    Added generator func to reactome

--- pypath/inputs/reactome/__init__.py
+++ pypath/inputs/reactome/__init__.py
@@ -1,7 +1,13 @@
 # ChEBI2Reactome_PE_All_Levels: https://reactome.org/download/current/ChEBI2Reactome_PE_All_Levels.txt
 # pathway hierarchy: https://reactome.org/download/current/ReactomePathwaysRelation.txt
+import csv
+from collections.abc import Generator
 from pypath.resources import urls
+from pypath.share import curl
 
 def reactome_raw(fname: str) -> Generator[tuple]:
 
-    url = urls.urls['reactomr']['txt'] % fname
+    url = urls.urls['reactome']['txt'] % fname
+
+    c = curl.Curl(url, large=True)
+    yield from csv.DictReader(c.fileobj)
