581662a445364592e43f0f1fc58979f4598241d9
Macabe Daley <37773554+maca8e@users.noreply.github.com>
2025-02-11 16:16:19 +0100

    Inputs reactome, takes colnames, delimiters

--- pypath/inputs/reactome/__init__.py
+++ pypath/inputs/reactome/__init__.py
@@ -5,9 +5,16 @@ from collections.abc import Generator
 from pypath.resources import urls
 from pypath.share import curl
 
-def reactome_raw(fname: str) -> Generator[tuple]:
+def _reactome_txt(fname: str, colnames: list[str]) -> Generator[tuple]:
 
     url = urls.urls['reactome']['txt'] % fname
 
     c = curl.Curl(url, large=True)
-    yield from csv.DictReader(c.fileobj)
+    yield from csv.DictReader(c.fileobj, delimiter='\t', fieldnames=colnames)
+
+
+def reactome_raw():
+    colnames = ['id', 'pathway_id', 'url', 'pathway_name', 'evidence_code', 'species']
+    yield from _reactome_txt(fname='ChEBI2Reactome_PE_All_Levels', colnames=colnames)
+
+
