1697a409e86248ea3087bbf7eb3db3318f467098
Macabe Daley <37773554+maca8e@users.noreply.github.com>
2025-02-11 15:32:10 +0100

    New reactome client URLs

---
+++ pypath/inputs/reactome/__init__.py
@@ -0,0 +1,2 @@
+# ChEBI2Reactome_PE_All_Levels: https://reactome.org/download/current/ChEBI2Reactome_PE_All_Levels.txt
+# pathway hierarchy: https://reactome.org/download/current/ReactomePathwaysRelation.txt
