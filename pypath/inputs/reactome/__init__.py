56fdff23ebebaff94f833efd23c1faa2245c3899
Macabe Daley <37773554+maca8e@users.noreply.github.com>
2025-02-11 16:48:50 +0100

    Specified reactome raw parameter parsing

--- pypath/inputs/reactome/__init__.py
+++ pypath/inputs/reactome/__init__.py
@@ -1,6 +1,7 @@
 # ChEBI2Reactome_PE_All_Levels: https://reactome.org/download/current/ChEBI2Reactome_PE_All_Levels.txt
 # pathway hierarchy: https://reactome.org/download/current/ReactomePathwaysRelation.txt
 import csv
+from typing import Literal
 from collections.abc import Generator
 from pypath.resources import urls
 from pypath.share import curl
@@ -16,7 +17,7 @@ def _reactome_txt(fname: str, colnames: list[str]) -> Generator[tuple]:
 def reactome_raw(
         id_type: str,
         physical_entities: bool = False,
-        event_type: Literal['all', 'lowest', 'reaction'],
+        event_type: Literal['all', 'lowest', 'reaction'] = 'all',
     ) -> Generator[tuple]:
 
     IDTYPES = {
@@ -28,17 +29,20 @@ def reactome_raw(
         'chebi': 'ChEBI',
     }
     EVENT_TYPES = {
-        'lowest': 'Pathway',
-        'all': 'All_levels',
+        'lowest': '',
+        'all': '_All_Levels',
         'reaction': 'Reactions',
     }
 
     colnames = ['id', 'pathway_id', 'url', 'pathway_name', 'evidence_code', 'species']
+    id_type = IDTYPES.get(id_type, id_type)
+    event_type = EVENT_TYPES.get(event_type, event_type)
+    physical_entities = '_PE_' if physical_entities else ''
 
-    fname = f'{id_type}2Reactome{pe}{event_type}'
+    fname = f'{id_type}2Reactome{physical_entities}{event_type}'
 
     yield from _reactome_txt(
-        fname = 'ChEBI2Reactome_PE_All_Levels',
+        fname = fname,
         colnames = colnames,
     )
 
