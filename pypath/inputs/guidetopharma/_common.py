9263970281b7dc0b4c823c58d5784b739a85572a
TobyL98 <tobylawrence@btinternet.com>
2025-03-31 12:24:35 +0100

    Included all csv format datasets for guide to pharmacology

--- pypath/inputs/guidetopharma/_common.py
+++ pypath/inputs/guidetopharma/_common.py
@@ -1,11 +1,26 @@
+from typing import Literal
 from collections.abc import Generator
 
 import pypath.share.curl as curl
 import pypath.share.common as common
 import pypath.resources.urls as urls
 
+TABLES = Literal[
+    'ligands',
+    'interactions',
+    'targets_and_families',
+    'ligand_id_mapping',
+    'ligand_physchem_properties',
+    'endogenous_ligand_pairings_all',
+    'endogenous_ligand_detailed',
+    'approved_drug_primary_target_interactions',
+    'approved_drug_detailed_interactions',
+    'peptides',
+    'GtP_to_HGNC_mapping',
+    'GtP_to_UniProt_mapping'
+]
 
-def guide2pharma_table(name: str) -> Generator[dict]:
+def guide2pharma_table(name: TABLES) -> Generator[dict]:
     """
     Downloads the table from Guide2Pharma.
 
