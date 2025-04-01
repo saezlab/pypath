0fa7bc9690c0be0b1dcb9bc42c888f364093a69d
TobyL98 <tobylawrence@btinternet.com>
2025-04-01 11:50:25 +0100

    'guidetopharma': function for returning ligand information

--- pypath/inputs/guidetopharma/_common.py
+++ pypath/inputs/guidetopharma/_common.py
@@ -10,76 +10,76 @@ import pypath.utils.taxonomy as taxonomy
 import pypath.resources.urls as urls
 
 TABLES = Literal[
-    'ligands',
-    'interactions',
-    'targets_and_families',
-    'ligand_id_mapping',
-    'ligand_physchem_properties',
-    'endogenous_ligand_pairings_all',
-    'endogenous_ligand_detailed',
-    'approved_drug_primary_target_interactions',
-    'approved_drug_detailed_interactions',
-    'peptides',
-    'GtP_to_HGNC_mapping',
-    'GtP_to_UniProt_mapping'
+    "ligands",
+    "interactions",
+    "targets_and_families",
+    "ligand_id_mapping",
+    "ligand_physchem_properties",
+    "endogenous_ligand_pairings_all",
+    "endogenous_ligand_detailed",
+    "approved_drug_primary_target_interactions",
+    "approved_drug_detailed_interactions",
+    "peptides",
+    "GtP_to_HGNC_mapping",
+    "GtP_to_UniProt_mapping",
 ]
 
 POSITIVE_REGULATION = {
-    'agonist',
-    'activator',
-    'potentiation',
-    'partial agonist',
-    'inverse antagonist',
-    'full agonist',
-    'activation',
-    'irreversible agonist',
-    'positive',
-    'biased agonist',
-    'slows inactivation',
+    "agonist",
+    "activator",
+    "potentiation",
+    "partial agonist",
+    "inverse antagonist",
+    "full agonist",
+    "activation",
+    "irreversible agonist",
+    "positive",
+    "biased agonist",
+    "slows inactivation",
 }
 NEGATIVE_REGULATION = {
-    'inhibitor',
-    'antagonist',
-    'competitive',
-    'feedback inhibition',
-    'inhibition',
-    'irreversible inhibition',
-    'inverse agonist',
-    'negative',
-    'weak inhibition',
-    'reversible inhibition',
-    'voltage-dependent inhibition',
-    'pore blocker',
+    "inhibitor",
+    "antagonist",
+    "competitive",
+    "feedback inhibition",
+    "inhibition",
+    "irreversible inhibition",
+    "inverse agonist",
+    "negative",
+    "weak inhibition",
+    "reversible inhibition",
+    "voltage-dependent inhibition",
+    "pore blocker",
 }
 
 
 G2PInteraction = collections.namedtuple(
-    'G2PInteraction',
+    "G2PInteraction",
     [
-        'is_stimulation',
-        'is_inhibition',
-        'ligand',
-        'endogenous',
+        "is_stimulation",
+        "is_inhibition",
+        "ligand",
+        "endogenous",
     ],
 )
 
 G2PLigand = collections.namedtuple(
-    'G2PLigand',
+    "G2PLigand",
     [
-        'name',
-        'pubchem',
-        'chembl',
-        'iupac',
-        'smiles',
-        'inchi',
+        "name",
+        "pubchem",
+        "chembl",
+        "iupac",
+        "smiles",
+        "inchi",
     ],
 )
 
 G2PProtein = collections.namedtuple(
-    'G2PProtein',
+    "G2PProtein",
     [
-        'uniprot',
-    ]
+        "uniprot",
+    ],
 )
 
 
@@ -95,9 +95,9 @@ def guide2pharma_table(name: TABLES) -> Generator[dict]:
         Generator yielding the table as named tuples.
     """
 
-    url = urls.urls['gtp']['url'] % name
+    url = urls.urls["gtp"]["url"] % name
 
-    c = curl.Curl(url, silent = False, large = True, encoding = 'utf-8')
+    c = curl.Curl(url, silent=False, large=True, encoding="utf-8")
 
     g2p_version = next(c.result).strip()
 
@@ -105,9 +105,9 @@ def guide2pharma_table(name: TABLES) -> Generator[dict]:
 
 
 def guide2pharma_interactions(
-        organism: str | int | None = 'human',
-        endogenous: bool | None = None,
-    ) -> Generator[tuple]:
+    organism: str | int | None = "human",
+    endogenous: bool | None = None,
+) -> Generator[tuple]:
     """
     Args:
         organism
@@ -120,40 +120,52 @@ def guide2pharma_interactions(
 
     get_taxid = lambda x: (
         _const.NOT_ORGANISM_SPECIFIC
-            if x in {'', 'None', None} else
-        taxonomy.ensure_ncbi_tax_id(x)
+        if x in {"", "None", None}
+        else taxonomy.ensure_ncbi_tax_id(x)
     )
     organism_ = None
     ncbi_tax_id = None
 
     if isinstance(organism, str):
-
         ncbi_tax_id = get_taxid(organism)
 
         try:
-
             organism_ = taxonomy.ensure_common_name(ncbi_tax_id)
             organism_ = organism_.capitalize() if organism_ else None
 
         except KeyError:
-
             pass  # no organism specified
 
-    for row in guide2pharma_table('interactions'):
+    for row in guide2pharma_table("interactions"):
+        _endogenous = row["Endogenous"].lower() == "true"
 
-        _endogenous = row['Endogenous'].lower() == "true"
-
-        if (
-            _endogenous is not None and
-            endogenous != _endogenous
-        ):
-
+        if _endogenous is not None and endogenous != _endogenous:
             continue
 
         yield G2PInteraction(
-            is_stimulation = row['Action'].lower() in POSITIVE_REGULATION,
-            is_inhibition = row['Action'].lower() in NEGATIVE_REGULATION,
-            ligand = row['Ligand ID'],
-            endogenous = _endogenous,
-
+            is_stimulation=row["Action"].lower() in POSITIVE_REGULATION,
+            is_inhibition=row["Action"].lower() in NEGATIVE_REGULATION,
+            ligand=row["Ligand ID"],
+            endogenous=_endogenous,
         )
+
+
+def guide2pharma_ligands() -> dict[str, G2PLigand]:
+    """
+    Downloads ligands from Guide2Pharma.
+
+    Yields:
+        Namedtuples containing name, PubChem ID, ChEMBL ID, IUPAC name, SMILES,
+        and InChI of each ligand.
+    """
+    return {
+        row["Ligand ID"]: G2PLigand(
+            name=row["name"],
+            pubchem=row["PubChem CID"],
+            chembl=row["ChEMBL ID"],
+            iupac=row["IUPAC Name"],
+            smiles=row["SMILES"],
+            inchi=row["InChI"],
+        )
+        for row in guide2pharma_table("ligands")
+    }
