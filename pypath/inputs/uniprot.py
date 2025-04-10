a6b1ec3c0203b26ed66d1d89a062173c6724e14b
deeenes <turei.denes@gmail.com>
2025-04-10 15:53:55 +0200

    `UniprotQuery`: strip also semicolons from the end of each result field

--- pypath/inputs/uniprot.py
+++ pypath/inputs/uniprot.py
@@ -374,7 +374,7 @@ class UniprotQuery:
     _OPSTART = re.compile(r'^(OR|AND)')
     _OPEND = re.compile(r'(OR|AND)$')
     _FIELDSEP = re.compile(r'[\s;]')
-    _FIELDEND = re.compile(r'$;')
+    _FIELDEND = re.compile(r';$')
     _SYNONYMS = {
         'organism': 'organism_id',
         'ncbi_tax_id': 'organism_id',
@@ -660,7 +660,7 @@ class UniprotQuery:
 
         for line in result:
 
-            line = line.strip('\n\r;')
+            line = line.strip('\n\r')
 
             if line.strip():
 
