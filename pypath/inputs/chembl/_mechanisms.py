0c866d1b40cb1343fea6cb24f23ea32cc0e2053f
TobyL98 <tobylawrence@btinternet.com>
2025-04-25 22:12:09 +0100

    'inputs._mechanisms': changes mechanisms name to mech

--- pypath/inputs/chembl/_mechanisms.py
+++ pypath/inputs/chembl/_mechanisms.py
@@ -14,19 +14,19 @@ def mechanism(max_pages: int | None = None) -> Generator[ChemblMechanism]:
 
     mechanisms = _raw.json_pages(data_type="mechanism", max_pages=max_pages)
 
-    for mechanism in mechanisms:
+    for mech in mechanisms:
 
         # Get the mechanism references
-        references = [ref["ref_url"] for ref in mechanism["mechanism_refs"]]
+        references = [ref["ref_url"] for ref in mech["mechanism_refs"]]
 
         yield ChemblMechanism(
-            action_type = mechanism["action_type"],
-            molecule_chembl_id = mechanism["molecule_chembl_id"],
-            target_chembl_id = mechanism["target_chembl_id"],
-            mechanism_id = mechanism["mec_id"],
-            drug_rec_id = mechanism["record_id"],
-            direct_interaction = mechanism["direct_interaction"],
-            variant_sequence = mechanism["variant_sequence"],
-            molecular_mechanism = mechanism["molecular_mechanism"],
+            action_type = mech["action_type"],
+            molecule_chembl_id = mech["molecule_chembl_id"],
+            target_chembl_id = mech["target_chembl_id"],
+            mechanism_id = mech["mec_id"],
+            drug_rec_id = mech["record_id"],
+            direct_interaction = mech["direct_interaction"],
+            variant_sequence = mech["variant_sequence"],
+            molecular_mechanism = mech["molecular_mechanism"],
             mechanism_refs = references,
         )
