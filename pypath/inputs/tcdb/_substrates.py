c24ecdadd936604af9c20b19924eee6813067dfc
deeenes <turei.denes@gmail.com>
2025-07-10 10:31:35 +0200

    `tcdb`: query the locations for all uniprot at once

--- pypath/inputs/tcdb/_substrates.py
+++ pypath/inputs/tcdb/_substrates.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 TcdbSubstrate = collections.namedtuple(
     'TcdbSubstrate',
-    ['transporter_uniprot', 'substrate_id', 'substrate_name','location','features'],
+    ['transporter_uniprot', 'substrate_id', 'substrate_name', 'location'],
 )
 
 SLCSubstrate = collections.namedtuple(
@@ -16,36 +16,41 @@ SLCSubstrate = collections.namedtuple(
 )
 
 def tcdb_substrate() -> Generator[TcdbSubstrate, None, None]:
+
     url_substrates = urls.urls['tcdb']['url_substrates']
     c = curl.Curl(url_substrates, large = True)
-    TC2Sub = c.result
+    tc_to_substrate = c.result
     url_acc2tc = urls.urls['tcdb']['url_acc2tc']
     c = curl.Curl(url_acc2tc, large = True)
-    TC2Uni = c.result
+    tc_to_uniprot = c.result
+
     #ID conversion
-    dic_TCDB_Uni = collections.defaultdict(list)
-    for line in TC2Uni:
+    tcdb_uniprot = collections.defaultdict(list)
+    for line in tc_to_uniprot:
         uniprot_symbol, tcdb = line.strip('\n').split('\t')
         if uniprot_symbol:
-            dic_TCDB_Uni[tcdb].append(uniprot_symbol)
-    for line in TC2Sub:
+            tcdb_uniprot[tcdb].append(uniprot_symbol.upper())
+
+    all_uniprots = set().union(*tcdb_uniprot.values())
+
+    uniprot_locations = uniprot.uniprot_locations(
+        accession=all_uniprots,
+        organism=None,
+    )
+
+    for line in tc_to_substrate:
         tcdb, substrates = line.strip('\n').split('\t')
         for substrate in substrates.split('|'):
             substrate_id, substrate_name = substrate.split(';')
-            for transporter_uniprot in dic_TCDB_Uni[tcdb]:
-                try:
-                    uploc = uniprot.uniprot_locations(accession=transporter_uniprot.upper(), organism=None)
-                    if any(uploc):
-                        loc = list(uploc[transporter_uniprot.upper()])[0]
-                        location = loc.location
-                        features = loc.features
-                    else:
-                        location = None
-                        features = None
-                except ValueError:
-                    location = None
-                    features = None
-                yield TcdbSubstrate(transporter_uniprot, substrate_id, substrate_name,location,features)
+            for transporter_uniprot in tcdb_uniprot[tcdb]:
+
+                yield TcdbSubstrate(
+                    transporter_uniprot,
+                    substrate_id,
+                    substrate_name,
+                    uniprot_locations.get(transporter_uniprot),
+                )
+
 
 def slc_table() -> Generator[SLCSubstrate, None, None]:
     url = "https://www.embopress.org/action/downloadSupplement?doi=10.15252%2Fmsb.20209652&file=msb209652-sup-0003-TableEV2.xlsx"
