0754bfbfa83d28884ffb48fbbd966fe3cb631643
deeenes <turei.denes@gmail.com>
2025-08-12 02:06:15 +0200

    `inputs.slc`: access functions to raw tables

--- pypath/inputs/slc.py
+++ pypath/inputs/slc.py
@@ -1,8 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-#SLC table
-import requests
-url = "https://www.embopress.org/action/downloadSupplement?doi=10.15252%2Fmsb.20209652&file=msb209652-sup-0003-TableEV2.xlsx"
-c = curl.Curl(url)
-SLC_table = c.result
+#
+#  This file is part of the `pypath` python module
+#
+#  Copyright 2014-2023
+#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
+#
+#  Authors: see the file `README.rst`
+#  Contact: Dénes Türei (turei.denes@gmail.com)
+#
+#  Distributed under the GPLv3 License.
+#  See accompanying file LICENSE.txt or copy at
+#      https://www.gnu.org/licenses/gpl-3.0.html
+#
+#  Website: https://pypath.omnipathdb.org/
+#
 
-url = "https://www.embopress.org/action/downloadSupplement?doi=10.15252%2Fmsb.20209652&file=msb209652-sup-0002-TableEV1.xlsx"
+import pypath.share.curl as curl
+import pypath.resources.urls as urls
+import pypath.inputs.common as inputs_common
+
+
+def _slc_raw(table = 1, sheet = 1):
+
+    url = urls.urls['slc'][f'table_s{table}']
+    c = curl.Curl(url, large = True, silent = False)
+    content = inputs_common.read_xls(c.fileobj.name, sheet = sheet)
+
+    return content
+
+
+def slc_annotation():
+
+    return _slc_raw(table = 1, sheet = 1)
+
+
+def slc_substrate_ontology():
+
+    return _slc_raw(table = 1, sheet = 2)
+
+
+def slc_chebi_mapping():
+
+    return _slc_raw(table = 2, sheet = 1)
