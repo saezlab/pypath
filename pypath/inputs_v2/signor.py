a557672d8e75ebfb6d30efea570c5005b4f4b0c5
Jonathan Schaul <46248679+jonsch1@users.noreply.github.com>
2025-12-10 07:35:02 +0100

    .

--- pypath/inputs_v2/signor.py
+++ pypath/inputs_v2/signor.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#
-#  This file is part of the `pypath` python module
-#
-#  Copyright 2014-2025
-#  EMBL, EMBL-EBI, Uniklinik RWTH Aachen, Heidelberg University
-#
-#  Authors: see the file `README.rst`
-#  Contact: Dénes Türei (turei.denes@gmail.com)
-#
-#  Distributed under the GPLv3 License.
-#  See accompanying file LICENSE.txt or copy at
-#      https://www.gnu.org/licenses/gpl-3.0.html
-#
-#  Website: https://pypath.omnipathdb.org/
-#
-
 """
 Parse SIGNOR data and emit Entity records.
 
@@ -98,9 +79,6 @@ def signor_complexes() -> Generator[Entity]:
         identifiers=IdentifiersBuilder(
             CV(term=IdentifierNamespaceCv.SIGNOR, value=Column('SIGNOR ID')),
             CV(term=IdentifierNamespaceCv.NAME, value=Column('COMPLEX NAME')),
-        ),
-        annotations=AnnotationsBuilder(
-            # Source annotation
         ),
         membership=MembershipBuilder(
             MembersFromList(
