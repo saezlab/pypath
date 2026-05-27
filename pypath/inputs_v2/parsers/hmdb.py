303812166264ff34b86d091024e64399117e50a2
Nicolas Palacio <nicolaspalacio91@gmail.com>
2026-05-27 13:21:26 +0200

    Unified solution to parse HMDB XML tree

--- pypath/inputs_v2/parsers/hmdb.py
+++ pypath/inputs_v2/parsers/hmdb.py
@@ -36,20 +36,29 @@ def _raw(opener, max_records: int | None = None, **_kwargs: object) -> Generator
 
     xmlns = '{http://www.hmdb.ca}'
 
-    def get_text(elem: etree._Element, tag: str, parent: etree._Element = None) -> str:
-        parent = parent if parent is not None else elem
-        child = parent.find(f'{xmlns}{tag}')
-        return child.text.strip() if child is not None and child.text else ''
+    # XXX: Testing if get_from_path can substitute all use-cases
 
-    def get_list(elem: etree._Element, container_tag: str, item_tag: str) -> str:
-        container = elem.find(f'{xmlns}{container_tag}')
-        if container is None:
-            return ''
-        items = [
-            item.text.strip() for item in container.findall(f'{xmlns}{item_tag}')
-            if item.text and item.text.strip()
-        ]
-        return ';'.join(items)
+    #def get_text(elem: etree._Element, tag: str, parent: etree._Element = None) -> str:
+    #    parent = parent if parent is not None else elem
+    #    child = parent.find(f'{xmlns}{tag}')
+    #    return child.text.strip() if child is not None and child.text else ''
+
+    #def get_list(elem: etree._Element, container_tag: str, item_tag: str) -> str:
+    #    container = elem.find(f'{xmlns}{container_tag}')
+    #    if container is None:
+    #        return ''
+    #    items = [
+    #        item.text.strip() for item in container.findall(f'{xmlns}{item_tag}')
+    #        if item.text and item.text.strip()
+    #    ]
+    #    return ';'.join(items)
+
+    def get_from_path(elem, *args):
+
+        path = '/'.join([f'{xmlns}{a}' for a in args])
+        res = [e.text.strip() for e in elem.findall(path)]
+
+        return ';'.join(res)
 
     context = etree.iterparse(xml_file, events=('end',), tag=f'{xmlns}metabolite')
 
@@ -58,30 +67,49 @@ def _raw(opener, max_records: int | None = None, **_kwargs: object) -> Generator
         if max_records is not None and count >= max_records:
             break
 
-        pubmed_ids = []
-        general_refs = elem.find(f'{xmlns}general_references')
-        if general_refs is not None:
-            for ref in general_refs.findall(f'{xmlns}reference'):
-                pubmed_id = ref.find(f'{xmlns}pubmed_id')
-                if pubmed_id is not None and pubmed_id.text:
-                    pubmed_ids.append(pubmed_id.text.strip())
+        #pubmed_ids = []
+        #general_refs = elem.find(f'{xmlns}general_references')
+        #if general_refs is not None:
+        #    for ref in general_refs.findall(f'{xmlns}reference'):
+        #        pubmed_id = ref.find(f'{xmlns}pubmed_id')
+        #        if pubmed_id is not None and pubmed_id.text:
+        #            pubmed_ids.append(pubmed_id.text.strip())
 
         yield {
-            'accession': get_text(elem, 'accession'),
-            'name': get_text(elem, 'name'),
-            'traditional_iupac': get_text(elem, 'traditional_iupac'),
-            'iupac_name': get_text(elem, 'iupac_name'),
-            'synonyms': get_list(elem, 'synonyms', 'synonym'),
-            'inchikey': get_text(elem, 'inchikey'),
-            'inchi': get_text(elem, 'inchi'),
-            'smiles': get_text(elem, 'smiles'),
-            'chebi_id': get_text(elem, 'chebi_id'),
-            'pubchem_compound_id': get_text(elem, 'pubchem_compound_id'),
-            'kegg_id': get_text(elem, 'kegg_id'),
-            'drugbank_id': get_text(elem, 'drugbank_id'),
-            'cas_registry_number': get_text(elem, 'cas_registry_number'),
-            'description': get_text(elem, 'description'),
-            'pubmed_ids': ';'.join(pubmed_ids) if pubmed_ids else '',
+            #'accession': get_text(elem, 'accession'),
+            #'name': get_text(elem, 'name'),
+            #'traditional_iupac': get_text(elem, 'traditional_iupac'),
+            #'iupac_name': get_text(elem, 'iupac_name'),
+            #'synonyms': get_list(elem, 'synonyms', 'synonym'),
+            #'inchikey': get_text(elem, 'inchikey'),
+            #'inchi': get_text(elem, 'inchi'),
+            #'smiles': get_text(elem, 'smiles'),
+            #'chebi_id': get_text(elem, 'chebi_id'),
+            #'pubchem_compound_id': get_text(elem, 'pubchem_compound_id'),
+            #'kegg_id': get_text(elem, 'kegg_id'),
+            #'drugbank_id': get_text(elem, 'drugbank_id'),
+            #'cas_registry_number': get_text(elem, 'cas_registry_number'),
+            #'description': get_text(elem, 'description'),
+            #'pubmed_ids': ';'.join(pubmed_ids) if pubmed_ids else '',
+            'accession': get_from_path(elem, 'accession'),
+            'name': get_from_path(elem, 'name'),
+            'traditional_iupac': get_from_path(elem, 'traditional_iupac'),
+            'iupac_name': get_from_path(elem, 'iupac_name'),
+            'synonyms': get_from_path(elem, 'synonyms', 'synonym'),
+            'inchikey': get_from_path(elem, 'inchikey'),
+            'inchi': get_from_path(elem, 'inchi'),
+            'smiles': get_from_path(elem, 'smiles'),
+            'chebi_id': get_from_path(elem, 'chebi_id'),
+            'pubchem_compound_id': get_from_path(elem, 'pubchem_compound_id'),
+            'kegg_id': get_from_path(elem, 'kegg_id'),
+            'drugbank_id': get_from_path(elem, 'drugbank_id'),
+            'cas_registry_number': get_from_path(elem, 'cas_registry_number'),
+            'description': get_from_path(elem, 'description'),
+            'pubmed_ids': get_from_path(elem, 'general_references', 'reference', 'pubmed_id'),
+            'diseases': get_from_path(elem, 'diseases', 'disease', 'name'),
+            'cellular_locations': get_from_path(elem, 'biological_properties', 'cellular_locations', 'cellular'),
+            'tissue_locations': get_from_path(elem, 'biological_properties', 'tissue_locations', 'tissue'),
+            'biospecimen_locations': get_from_path(elem, 'biological_properties', 'biospecimen_locations', 'biospecimen'),
         }
 
         count += 1
