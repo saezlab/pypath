768e00e6cb6fa45d1ffa3f63b37e86c40175f6c0
Nicolas Palacio <nicolaspalacio91@gmail.com>
2026-07-09 15:48:38 +0200

    Started adding hormone2cell to inputs v2 (WIP)

---
+++ pypath/inputs_v2/hormone2cell.py
@@ -0,0 +1,85 @@
+"""
+Parse Hormone2Cell data and emit Entity records.
+
+This module converts hormone-cell interaction information from Hormone2Cell into
+Entity records using the declarative schema pattern.
+"""
+
+from __future__ import annotations
+
+from pypath.inputs_v2.base import (
+    ResourceConfig,
+    Download,
+    Resource,
+    Dataset,
+    ontology_entity_mapper,
+)
+from pypath.inputs_v2.parsers import brenda as _parsers
+from pypath.internals.tabular_builder import (
+    AssociationBuilder,
+    AssociationsBuilder,
+    CV,
+    EntityBuilder,
+    FieldConfig,
+    IdentifiersBuilder,
+)
+from pypath.internals.cv_terms import (
+    EntityTypeCv,
+    IdentifierNamespaceCv,
+    LicenseCV,
+    OntologyCv,
+    UpdateCategoryCV,
+    ResourceCv,
+)
+
+# =================================== SET-UP ===================================
+
+URL = 'https://hormonecellatlas.org.uk/accounts/downloads/file/table-s2b/'
+
+config = ResourceConfig(
+    id=ResourceCv.HORMONE2CELL,
+    name='Hormone2Cell',
+    url='https://hormonecellatlas.org.uk/',
+    license=LicenseCV.UNSPECIFIED,
+    update_category=UpdateCategoryCV.REGULAR,
+    pubmed='42207862',
+    primary_category='intercell',
+    description=(
+        'The Hormone Cell Atlas uses single cell transcriptomics to map '
+        'hormone production and action across 47 disease-free human tissues at '
+        'cellular resolution.'
+    ),
+)
+
+
+download = Download(
+    url=URL,
+    filename='hormone2cell.xlsx',
+    subfolder='hormone2cell',
+    large=True,
+    ext='xlsx',
+    default_mode='rb',
+)
+
+# =================================== SCHEMA ===================================
+
+f = FieldConfig(
+    extract={},
+    map={},
+    transform={},
+)
+
+schema = EntityBuilder()
+
+# ================================= RESOURCE ===================================
+
+#resource = Resource(
+#    config=config,
+#    data=Dataset(
+#        download=download,
+#        mapper=schema,
+#        raw_parser=parser,
+#    ),
+#)
+
+# ================================= REFERENCE ==================================
